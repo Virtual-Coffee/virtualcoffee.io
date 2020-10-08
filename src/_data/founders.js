bd52f172b63a7c7a82cff3b15087c5ee47171130
Chris Jarvis <clj@ollure.com>
2020-10-07 20:08:03 -0500

    Founders Profiles (#47)
    * created founders and moved founding menbers to new folder, added founders.js
    
    * founders now show above members. added HR and h5 to mark as seperate groups.
    
    * after rebase
    
    * moved our members above hr line
    
    * changed section header to match style of other header.

---
+++ src/_data/founders.js
@@ -0,0 +1,35 @@
+const fetch = require('node-fetch');
+var glob = require('glob');
+var fs = require('fs');
+const path = require('path');
+const Cache = require('@11ty/eleventy-cache-assets');
+
+module.exports = async function () {
+  console.log('Fetching github users');
+  const founders = [];
+  const files = glob.sync(`${path.resolve(__dirname, 'founders')}/*.json`);
+
+  for (let i = 0; i < files.length; i++) {
+    const file = files[i];
+
+    try {
+      const data = fs.readFileSync(file, 'utf8');
+
+      var obj = JSON.parse(data);
+
+      const response = await Cache(
+        `https://api.github.com/users/${obj.username}`,
+        {
+          duration: '1d', // 1 day
+          type: 'json', // also supports "text" or "buffer"
+        }
+      );
+
+      founders.push(response);
+    } catch (e) {
+      console.warn(`Unable to read ${file}, skipping and moving on!`);
+    }
+  }
+
+  return founders;
+};
