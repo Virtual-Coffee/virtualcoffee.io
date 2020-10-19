70c11926ae15e37a0f5f30b8b78759ebe821b746
Dan Ott <dan@dtott.com>
2020-10-19 14:39:12 -0400

    formatting

--- src/_data/events.js
+++ src/_data/events.js
@@ -1,7 +1,7 @@
 const Cache = require('@11ty/eleventy-cache-assets');
 
 module.exports = async function () {
-  console.log("Fetching events");   
+  console.log('Fetching events');
   try {
     const response = await Cache(
       `https://meetingplace.io/api/v1/group/virtual-coffee/events`,
@@ -9,46 +9,10 @@ module.exports = async function () {
         duration: '1d', // 1 day
         type: 'json', // also supports "text" or "buffer"
       }
-    )
+    );
     // return response.slice(0, 10);
     return response;
   } catch (e) {
     console.error(e);
   }
 };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
