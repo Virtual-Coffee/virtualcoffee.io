d1b59a17dfcce83c92c3df4a2a11f7383dce17d2
Dan Ott <dan@dtott.com>
2020-10-20 11:33:44 -0400

    remove unused date filters

--- .eleventy.js
+++ .eleventy.js
@@ -16,17 +16,6 @@ module.exports = function (eleventyConfig) {
 
   eleventyConfig.addLayoutAlias('post', 'layouts/post.njk');
 
-  eleventyConfig.addFilter('readableDate', (dateObj) => {
-    return DateTime.fromJSDate(dateObj, { zone: 'utc' }).toFormat(
-      'dd LLL yyyy'
-    );
-  });
-
-  // https://html.spec.whatwg.org/multipage/common-microsyntaxes.html#valid-date-string
-  eleventyConfig.addFilter('htmlDateString', (dateObj) => {
-    return DateTime.fromJSDate(dateObj, { zone: 'utc' }).toFormat('yyyy-LL-dd');
-  });
-
   const defaultDateOptions = {
     zone: 'edt',
   };
