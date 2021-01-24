7231d95e88b618b5454da6e34346ea10fe78bff9
Bekah Hawrot Weigel <rhawrot@gmail.com>
2021-01-24 01:07:00 -0500

    Add monthly challenge recommendations

--- src/monthlychallenges/jan-2021/index.11tydata.js
+++ src/monthlychallenges/jan-2021/index.11tydata.js
@@ -1,20 +1,8 @@
 module.exports = function () {
   const { challengedata } = require('./jan-2021.json');
 
-  let totals = {};
-
-  challengedata.forEach((challenge, i) => {
-    challenge.participants.forEach((p) => {
-      if (p in totals) {
-        totals[p] = totals[p] + 1;
-      } else {
-        totals[p] = 1;
-      }
-    });
-  });
 
   return {
-    list: challengedata,
-    totals,
+    list: challengedata
   };
 };
