5b5d4049aceeacfdde5ca0fcedfc915d237b203e
Bekah Hawrot Weigel <rhawrot@gmail.com>
2021-01-28 12:03:18 -0500

    Fix for

--- src/monthlychallenges/jan-2021/index.11tydata.js
+++ src/monthlychallenges/jan-2021/index.11tydata.js
@@ -1,8 +1,13 @@
 module.exports = function () {
   const { challengedata } = require('./jan-2021.json');
-
-
+  challengedata.forEach(function (person) {
+    const p = {
+      name: person.name
+    };
+  });
   return {
-    list: challengedata
+    list: challengedata,
+    sortedList: challengedata.sort((a, b) => a.name.localeCompare(b.name)),
+
   };
-};
+}
