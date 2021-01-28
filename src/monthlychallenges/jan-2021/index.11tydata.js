bbe4760aacf332605ff3446104f3565b28dc96c2
Dan Ott <dan@dtott.com>
2021-01-28 12:32:59 -0500

    remove extra exports

--- src/monthlychallenges/jan-2021/index.11tydata.js
+++ src/monthlychallenges/jan-2021/index.11tydata.js
@@ -1,13 +1,7 @@
 module.exports = function () {
   const { challengedata } = require('./jan-2021.json');
-  challengedata.forEach(function (person) {
-    const p = {
-      name: person.name
-    };
-  });
-  return {
-    list: challengedata,
-    sortedList: challengedata.sort((a, b) => a.name.localeCompare(b.name)),
 
+  return {
+    list: challengedata.sort((a, b) => a.name.localeCompare(b.name)),
   };
-}
+};
