192e5e77dc617ff3126a84877504b49c2b39d3c6
Dan Ott <dan@dtott.com>
2021-05-24 09:24:28 -0400

    Use episode ID to fetch transcript from buzzsprout

--- src/podcast/podcast.11tydata.js
+++ src/podcast/podcast.11tydata.js
@@ -1,7 +1,5 @@
 const ImgixClient = require('imgix-core-js');
-const fs = require('fs');
-const path = require('path');
-const querystring = require('querystring');
+const Cache = require('@11ty/eleventy-cache-assets');
 
 const useImgix =
   process.env.USE_IMGIX || process.env.ELEVENTY_ENV === 'production';
@@ -10,24 +8,22 @@ var imgixClient = new ImgixClient({
   domain: 'virtualcoffee.imgix.net',
 });
 
+const podID = '1558601';
+
+// /:podcast_id/:episode_id/transcript(.format)
 module.exports = {
   eleventyComputed: {
     metadata: (data) => {
       if (data.podcast) {
         try {
-          const { mp3, card } = data.podcast;
-          if (mp3 && card) {
-            const [urlKey, query] = mp3.split('.mp3?');
-            const queries = querystring.parse(query);
-            const blobID = queries.blob_id || data.page.fileSlug;
+          const { card, id } = data.podcast;
+          if (id && card) {
             return {
               ...data.metadata,
               tags: {
                 ...data.metadata.tags,
-                'twitter:player':
-                  urlKey +
-                  '?client_source=twitter_card&amp;player_type=full_screen',
-                'twitter:player:stream': `${urlKey}'.mp3?blob_id=${blobID}&client_source=twitter_card`,
+                'twitter:player': `https://www.buzzsprout.com/${podID}/${id}?client_source=twitter_card&amp;player_type=full_screen`,
+                'twitter:player:stream': `https://www.buzzsprout.com/${podID}/${id}.mp3?blob_id=${id}&client_source=twitter_card`,
                 'og:image': useImgix
                   ? imgixClient.buildURL(card, {
                       w: 250,
@@ -59,17 +55,14 @@ module.exports = {
     podcast: (data) => {
       if (data.podcast) {
         try {
-          const { mp3 } = data.podcast;
-          if (mp3) {
-            const [urlKey, query] = mp3.split('.mp3?');
-            const queries = querystring.parse(query);
-            const blobID = queries.blob_id || data.page.fileSlug;
+          const { id } = data.podcast;
+          if (id) {
             return {
               ...data.podcast,
-              blob_id: blobID,
-              playerSrc: `${urlKey}.js?container_id=buzzsprout-player-${blobID}&player=small`,
+              playerSrc: `https://www.buzzsprout.com/${podID}/${id}.js?container_id=buzzsprout-player-${id}&player=small`,
             };
           }
+
           return data.podcast;
         } catch (e) {
           console.log(e);
@@ -79,37 +72,52 @@ module.exports = {
       return data.podcast;
     },
 
-    transcript: (data) => {
+    transcript: async (data) => {
       // attach transcript
       try {
-        const filename = path.resolve(
-          __dirname,
-          data.page.inputPath.split('/').pop().split('.njk')[0] + '.txt'
-        );
-
-        if (fs.existsSync(filename)) {
-          // return fs.readFileSync(filename, 'utf8');
-          const lines = fs.readFileSync(filename, 'utf8').split('\n');
-          const r = [];
-          lines.forEach((line, i) => {
-            if (i % 3 === 0) {
-              const s = line.split('  ');
-              if (s[1]) {
-                r.push({
-                  name: s[0],
-                  timestamp: s[1],
-                });
+        if (data.podcast.id) {
+          try {
+            const response = await Cache(
+              `https://www.buzzsprout.com/${podID}/${data.podcast.id}/transcript.json`,
+              {
+                duration: '5d', // 1 day
+                type: 'json', // also supports "text" or "buffer"
               }
-            }
+            );
 
-            if (i % 3 === 1) {
-              if (r[Math.floor(i / 3)]) {
-                r[Math.floor(i / 3)].text = line;
-              }
-            }
-          });
+            if (response && response.segments) {
+              const date = new Date(0);
 
-          return r;
+              return response.segments.reduce((arr, segment) => {
+                if (
+                  arr.length &&
+                  arr[arr.length - 1].name === segment.speaker
+                ) {
+                  const cur = arr.pop();
+                  return [
+                    ...arr,
+                    {
+                      ...cur,
+                      text: cur.text + ' ' + segment.body,
+                    },
+                  ];
+                } else {
+                  date.setSeconds(segment.startTime);
+
+                  return [
+                    ...arr,
+                    {
+                      name: segment.speaker,
+                      text: segment.body,
+                      timestamp: date.toISOString().substr(14, 5),
+                    },
+                  ];
+                }
+              }, []);
+            }
+          } catch (error) {
+            console.log(`No transcript found for ${data.page.fileSlug}`);
+          }
         }
       } catch (error) {
         console.log(error);
