593a98f6ab190f2606ffc716c27151d90fe98fca
MCCurcio <mccurcio@users.noreply.github.com>
2021-10-01 12:39:06 -0400

    Removed comments to show Matt Curcio's info

--- src/_data/members/members/mccurcio.js
+++ src/_data/members/members/mccurcio.js
@@ -6,25 +6,25 @@ module.exports = {
   // you can override that data here, as well as provide some additional account links below
   //
   // name - if not defined here, will default to your Name on github if defined, if not, then your username
-  // name: 'Your Name',
+  name: 'Matthew Curcio',
   //
   // mainUrl: the url your name links to on the members page. defaults to your github profile
   // mainUrl: 'https://virtualcoffee.io',
   //
   // bio - accepts markdown.
-  // bio: `This is _my_ **bio** and [here is a link](https://virtualcoffee.io)`,
+  bio: `TBA`,
   //
   // can take  one of each type except website - you can add as many `website` accounts as you wish
   accounts: [
-    // { type: 'linkedin', username: 'mattcurcio' },
-    // { type: 'dev', username: 'mccurcio' },
-    // { type: 'codenewbie', username: 'mccurcio' },
-    // { type: 'twitter', username: '@oaxacamatt1' },
+    { type: 'linkedin', username: 'mattcurcio' },
+    { type: 'dev', username: 'mccurcio' },
+    { type: 'codenewbie', username: 'mccurcio' },
+    { type: 'twitter', username: '@oaxacamatt1' },
     // { type: 'twitch', username: 'yourUserName' },
     // { type: 'youtube', channelId: 'yourChannelId' }, // or { type: 'youtube', customUrl: 'https://www.youtube.com/c/yourCustomUrl' }
     // { type: 'polywork', username: 'yourUserName' },
     // { type: 'medium', username: 'yourUserName' },
     // { type: 'hashnode', username: 'yourUserName' },
-    // { type: 'website', url: 'https://github.com/mccurcio', title: 'Matthew Curcio' },
+    { type: 'website', url: 'https://github.com/mccurcio', title: 'Matthew Curcio' },
   ],
 };
