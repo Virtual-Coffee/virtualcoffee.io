4311e2b3bf785a7dbdc48c16307b32d84c89ea9b
Yusuf Taiwo Hassan <teehazzan@gmail.com>
2021-10-01 19:02:29 +0100

    Updated teezzan member details (#364)

--- src/_data/members/members/teezzan.js
+++ src/_data/members/members/teezzan.js
@@ -6,7 +6,7 @@ module.exports = {
   // you can override that data here, as well as provide some additional account links below
   //
   // name - if not defined here, will default to your Name on github if defined, if not, then your username
-  // name: 'Your Name',
+  name: 'Taiwo Yusuf',
   //
   // mainUrl: the url your name links to on the members page. defaults to your github profile
   // mainUrl: 'https://virtualcoffee.io',
@@ -16,15 +16,15 @@ module.exports = {
   //
   // can take  one of each type except website - you can add as many `website` accounts as you wish
   accounts: [
-    // { type: 'linkedin', username: 'yourlinkedinUserName' },
+    { type: 'linkedin', username: 'yusuf-hassan' },
     // { type: 'dev', username: 'yourUserName' },
     // { type: 'codenewbie', username: 'yourUserName' },
-    // { type: 'twitter', username: 'yourUserName' },
+    { type: 'twitter', username: 'TaiwoHY' },
     // { type: 'twitch', username: 'yourUserName' },
     // { type: 'youtube', channelId: 'yourChannelId' }, // or { type: 'youtube', customUrl: 'https://www.youtube.com/c/yourCustomUrl' }
-    // { type: 'polywork', username: 'yourUserName' },
+    { type: 'polywork', username: 'taiwohy' },
     // { type: 'medium', username: 'yourUserName' },
-    // { type: 'hashnode', username: 'yourUserName' },
+    { type: 'hashnode', username: 'TaiwoHY' },
     // { type: 'website', url: 'https://virtualcoffee.io', title: 'Title of link' },
   ],
 };
