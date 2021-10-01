d9a3db5b0bacd6c87d6a1784d2dda3d70bd5953d
secondl1ght <85003930+secondl1ght@users.noreply.github.com>
2021-10-01 13:14:49 -0600

    Update profile info
    Added a few new links!

--- src/_data/members/members/secondl1ght.js
+++ src/_data/members/members/secondl1ght.js
@@ -9,7 +9,7 @@ module.exports = {
   // name: 'Your Name',
   //
   // mainUrl: the url your name links to on the members page. defaults to your github profile
-  // mainUrl: 'https://virtualcoffee.io',
+  mainUrl: 'https://github.com/secondl1ght',
   //
   // bio - accepts markdown.
   // bio: `This is _my_ **bio** and [here is a link](https://virtualcoffee.io)`,
@@ -17,7 +17,7 @@ module.exports = {
   // can take  one of each type except website - you can add as many `website` accounts as you wish
   accounts: [
     // { type: 'linkedin', username: 'yourlinkedinUserName' },
-    // { type: 'dev', username: 'yourUserName' },
+    { type: 'dev', username: 'secondl1ght' },
     // { type: 'codenewbie', username: 'yourUserName' },
     // { type: 'twitter', username: 'yourUserName' },
     // { type: 'twitch', username: 'yourUserName' },
@@ -25,6 +25,6 @@ module.exports = {
     // { type: 'polywork', username: 'yourUserName' },
     // { type: 'medium', username: 'yourUserName' },
     // { type: 'hashnode', username: 'yourUserName' },
-    // { type: 'website', url: 'https://virtualcoffee.io', title: 'Title of link' },
+    { type: 'website', url: 'https://secondl1ght.site', title: 'Portfolio' },
   ],
 };
