53fa336824a732ba4ab3ef7bef1568a7d691c9af
Dan Ott <dan@dtott.com>
2021-09-30 21:19:47 -0400

    Feature/updated members (#355)
    * load more member data
    
    * cache data
    
    * style updates
    
    * add new account options
    
    * style updates
    
    * add new service icons
    
    * style updates
    
    * add new member data
    
    * update fake data
    
    * update examples
    
    * move lohit to new setup
    
    * remove old files

---
+++ src/_data/members/core/danieltott.js
@@ -0,0 +1,32 @@
+module.exports = {
+  // github username: required
+  github: 'danieltott',
+  //
+  // everything below here is optional.
+  //
+  // name: if not defined here, will default to your Name on github if defined, if not, then your username
+  name: 'Dan Ott',
+  // mainUrl: the url your name links to on the members page. defaults to your github profile
+  mainUrl: 'https://www.dtott.com',
+  // bio - accepts markdown.
+  bio: `Independent front-end designer and developer, father, #ADHD person, and Clevelander.
+
+  He/him`,
+  // can take  one of each type except website - you can add as many `website` accounts as you wish
+  accounts: [
+    { type: 'linkedin', username: 'danieltott' },
+    { type: 'dev', username: 'danieltott' },
+    { type: 'codenewbie', username: 'danieltott' },
+    { type: 'twitter', username: 'danieltott' },
+    { type: 'twitch', username: 'dtott' },
+    {
+      type: 'youtube',
+      channelId: 'UCc0579aGEy7jTAgRglR4J0g',
+      customUrl: 'https://www.youtube.com/c/VirtualCoffeeIO',
+    },
+    { type: 'polywork', username: 'danieltott' },
+    { type: 'medium', username: 'danieltott' },
+    { type: 'hashnode', username: 'danieltott' },
+    { type: 'website', url: 'https://clereact.dev', title: 'Cleveland React' },
+  ],
+};
