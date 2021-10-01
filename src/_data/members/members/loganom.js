b1b1049eafefd0a5f79200ca4e905f7183946ede
Logan McCamon <2434012+loganom@users.noreply.github.com>
2021-10-01 11:43:10 -0500

    add my member data

---
+++ src/_data/members/members/loganom.js
@@ -0,0 +1,32 @@
+module.exports = {
+    // github username: required
+    github: 'loganom',
+    //
+    // everything below here is optional. by default, we pull most profile data from your github profile.
+    // you can override that data here, as well as provide some additional account links below
+    //
+    // name - if not defined here, will default to your Name on github if defined, if not, then your username
+    name: 'Logan McCamon',
+    //
+    // mainUrl: the url your name links to on the members page. defaults to your github profile
+    // mainUrl: '',
+    //
+    // bio - accepts markdown.
+    bio: `Team Lead | Software Engineer with a data driven mindset and a passion for problem solving
+    
+    He/Him`,
+    // can take  one of each type except website - you can add as many `website` accounts as you wish
+    accounts: [
+      { type: 'linkedin', username: 'logan-mccamon' },
+      { type: 'dev', username: 'loganom' },
+      // { type: 'codenewbie', username: 'yourUserName' },
+      { type: 'twitter', username: 'LoganMcCamon' },
+      // { type: 'twitch', username: 'yourUserName' },
+      // { type: 'youtube', channelId: 'yourChannelId' }, // or { type: 'youtube', customUrl: 'https://www.youtube.com/c/yourCustomUrl' }
+      // { type: 'polywork', username: 'yourUserName' },
+      // { type: 'medium', username: 'yourUserName' },
+      // { type: 'hashnode', username: 'yourUserName' },
+      // { type: 'website', url: 'https://virtualcoffee.io', title: 'Title of link' },
+    ],
+  };
+  
