8dc5791b78a6510f36ff50ccfa4085c3d45efa54
Cassidy Mountjoy <43921118+CassidyMountjoy@users.noreply.github.com>
2021-10-04 11:01:03 -0700

    Members/cassidy mountjoy (#395)
    * Added Cassidy Mountjoy (me) to the members page
    
    * added cassidy mountjoy to members
    
    * added Cassidy Mountjoy to the members
    
    Co-authored-by: cmountjoy <cassidy.mountjoy@blockpoint.systems>

---
+++ src/_data/members/members/CassidyMountjoy.js
@@ -0,0 +1,30 @@
+module.exports = {
+  // github username: required
+  github: 'CassidyMountjoy',
+  //
+  // everything below here is optional. by default, we pull most profile data from your github profile.
+  // you can override that data here, as well as provide some additional account links below
+  //
+  // name - if not defined here, will default to your Name on github if defined, if not, then your username
+  name: 'Cassidy Mountjoy',
+  //
+  // mainUrl: the url your name links to on the members page. defaults to your github profile
+  mainUrl: 'https://cassidymountjoy.com/',
+  //
+  // bio - accepts markdown.
+  bio: `Entrepreneur and database developer [@Blockpoint](https://blockpointdb.com/), avid surfer and enjoyer of the outdoors`,
+  //
+  // can take  one of each type except website - you can add as many `website` accounts as you wish
+  accounts: [
+     { type: 'linkedin', username: 'cassidymountjoy' },
+     { type: 'dev', username: 'cassidymountjoy' },
+    // { type: 'codenewbie', username: 'yourUserName' },
+     { type: 'twitter', username: 'CptnMountjoy' },
+    // { type: 'twitch', username: 'yourUserName' },
+    // { type: 'youtube', channelId: 'yourChannelId' }, // or { type: 'youtube', customUrl: 'https://www.youtube.com/c/yourCustomUrl' }
+    // { type: 'polywork', username: 'yourUserName' },
+    // { type: 'medium', username: 'yourUserName' },
+    // { type: 'hashnode', username: 'yourUserName' },
+    // { type: 'website', url: 'https://virtualcoffee.io', title: 'Title of link' },
+  ],
+};
