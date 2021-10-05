582e926c3edacae77b69a1259728034f402bae8a
Nora Fergany <nfergany@andrew.cmu.edu>
2021-10-04 21:55:45 -0400

    Add norafergany member profile

---
+++ src/_data/members/members/norafergany.js
@@ -0,0 +1,31 @@
+module.exports = {
+  // github username: required
+  github: 'norafergany',
+  //
+  // everything below here is optional. by default, we pull most profile data from your github profile.
+  // you can override that data here, as well as provide some additional account links below
+  //
+  // name - if not defined here, will default to your Name on github if defined, if not, then your username
+  // name: 'Your Name',
+  //
+  // mainUrl: the url your name links to on the members page. defaults to your github profile
+  // mainUrl: 'https://virtualcoffee.io',
+  //
+  // bio - accepts markdown. Please keep your bio to a reasonable length, refer to
+  // [members page](https://virtualcoffee.io/members/) for examples
+  bio: `Full Stack Developer`,
+  //
+  // can take  one of each type except website - you can add as many `website` accounts as you wish
+  accounts: [
+    { type: 'linkedin', username: 'norafergany' },
+    // { type: 'dev', username: 'yourUserName' },
+    // { type: 'codenewbie', username: 'yourUserName' },
+    { type: 'twitter', username: 'norafergany' },
+    // { type: 'twitch', username: 'yourUserName' },
+    // { type: 'youtube', channelId: 'yourChannelId' }, // or { type: 'youtube', customUrl: 'https://www.youtube.com/c/yourCustomUrl' }
+    // { type: 'polywork', username: 'yourUserName' },
+    // { type: 'medium', username: 'yourUserName' },
+    // { type: 'hashnode', username: 'yourUserName' },
+    // { type: 'website', url: 'https://virtualcoffee.io', title: 'Title of link' },
+  ],
+};
