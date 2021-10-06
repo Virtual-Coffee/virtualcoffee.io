ba43d7b365a1243b2c5f7598f2303555072a596b
Matt McInnis (he/him) <mcinnis.matt@gmail.com>
2021-10-06 15:48:08 -0400

    Add official prettier settings 104 (#401)
    * updated prettier using --exact (as expressed in official docs) and created a config file
    
    * added base .prettierrc.json config
    
    * added and installed husky and lint-staged to enable prettier to run on commit
    
    * added a prettier ignore file
    
    * modifications to most members data files to prefer tabs over spaces.
    
    * more prettier reformatting of files, spaces to tabs
    
    * adjusted remaining files. mostly spaces to tabs and adding a few trailling commas
    
    * yarn.lock updated to reflect changes in package.json
    
    * Delete package-lock.json
    
    * add 'format' script to package.json to run prettier
    
    * created new glob pattern for prettier to also match non-prettier njk files. extended lint-staged config to do the same.
    
    * autoformatted njk files are updated
    
    * temporary commit
    
    * updated .editorconfig to prefer tab over space
    
    * fixed! all npx files updated, prettier settings implemented, all files are checked on commit.
    
    * updated format script and lint-staged settings in package.json
    
    Co-authored-by: Dan Ott <dan@dtott.com>

--- src/_data/members/members/nickytonline.js
+++ src/_data/members/members/nickytonline.js
@@ -1,34 +1,34 @@
 module.exports = {
-  // github username: required
-  github: 'nickytonline',
-  //
-  // everything below here is optional. by default, we pull most profile data from your github profile.
-  // you can override that data here, as well as provide some additional account links below
-  //
-  // name - if not defined here, will default to your Name on github if defined, if not, then your username
-  name: 'Nick Taylor',
-  //
-  // mainUrl: the url your name links to on the members page. defaults to your github profile
-  mainUrl: 'https://iamdeveloper.com',
-  //
-  // bio - accepts markdown.
-  // bio: `This is _my_ **bio** and [here is a link](https://virtualcoffee.io)`,
-  //
-  // can take  one of each type except website - you can add as many `website` accounts as you wish
-  accounts: [
-    // { type: 'linkedin', username: 'yourUserName' },
-    { type: 'dev', username: 'nickytonline' },
-    { type: 'codenewbie', username: 'nickytonline' },
-    { type: 'twitter', username: 'nickytonline' },
-    { type: 'twitch', username: 'nickytonline' },
-    { type: 'youtube', customUrl: 'https://youtube.iamdeveloper.com' },
-    { type: 'polywork', username: 'nickytonline' },
-    // { type: 'medium', username: 'yourUserName' },
-    { type: 'hashnode', username: 'nickytonline' },
-    {
-      type: 'website',
-      url: 'https://iamdeveloper.com',
-      title: 'Just Some Dev',
-    },
-  ],
+	// github username: required
+	github: 'nickytonline',
+	//
+	// everything below here is optional. by default, we pull most profile data from your github profile.
+	// you can override that data here, as well as provide some additional account links below
+	//
+	// name - if not defined here, will default to your Name on github if defined, if not, then your username
+	name: 'Nick Taylor',
+	//
+	// mainUrl: the url your name links to on the members page. defaults to your github profile
+	mainUrl: 'https://iamdeveloper.com',
+	//
+	// bio - accepts markdown.
+	// bio: `This is _my_ **bio** and [here is a link](https://virtualcoffee.io)`,
+	//
+	// can take  one of each type except website - you can add as many `website` accounts as you wish
+	accounts: [
+		// { type: 'linkedin', username: 'yourUserName' },
+		{ type: 'dev', username: 'nickytonline' },
+		{ type: 'codenewbie', username: 'nickytonline' },
+		{ type: 'twitter', username: 'nickytonline' },
+		{ type: 'twitch', username: 'nickytonline' },
+		{ type: 'youtube', customUrl: 'https://youtube.iamdeveloper.com' },
+		{ type: 'polywork', username: 'nickytonline' },
+		// { type: 'medium', username: 'yourUserName' },
+		{ type: 'hashnode', username: 'nickytonline' },
+		{
+			type: 'website',
+			url: 'https://iamdeveloper.com',
+			title: 'Just Some Dev',
+		},
+	],
 };
