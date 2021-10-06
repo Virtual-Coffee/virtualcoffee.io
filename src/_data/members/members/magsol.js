98a529799bd5185cf471bcca4b1ade0cc5927d6c
Shannon Quinn <magsol@gmail.com>
2021-10-06 09:55:42 -0400

    Created a membership file for myself.

--- src/_data/members/members/magsol.js
+++ src/_data/members/members/magsol.js
@@ -6,20 +6,21 @@ module.exports = {
   // you can override that data here, as well as provide some additional account links below
   //
   // name - if not defined here, will default to your Name on github if defined, if not, then your username
-  // name: 'Your Name',
+  name: 'Shannon Quinn',
   //
   // mainUrl: the url your name links to on the members page. defaults to your github profile
-  // mainUrl: 'https://virtualcoffee.io',
+  mainUrl: 'https://magsol.github.io',
   //
-  // bio - accepts markdown.
-  // bio: `This is _my_ **bio** and [here is a link](https://virtualcoffee.io)`,
+  // bio - accepts markdown. Please keep your bio to a reasonable length, refer to 
+  // [members page](https://virtualcoffee.io/members/) for examples
+  bio: 'Academic, Data Scientist, Runner, Donut-eater',
   //
   // can take  one of each type except website - you can add as many `website` accounts as you wish
   accounts: [
-    // { type: 'linkedin', username: 'yourlinkedinUserName' },
+    { type: 'linkedin', username: 'shannonpquinn' },
     // { type: 'dev', username: 'yourUserName' },
     // { type: 'codenewbie', username: 'yourUserName' },
-    // { type: 'twitter', username: 'yourUserName' },
+    { type: 'twitter', username: 'SpectralFilter' },
     // { type: 'twitch', username: 'yourUserName' },
     // { type: 'youtube', channelId: 'yourChannelId' }, // or { type: 'youtube', customUrl: 'https://www.youtube.com/c/yourCustomUrl' }
     // { type: 'polywork', username: 'yourUserName' },
