eb76504ced28843c8bb957632ba222f8d1cda361
andreamartz <14165788+andreamartz@users.noreply.github.com>
2021-10-09 14:11:35 -0500

    Update profile information for Andrea Martz

--- src/_data/members/members/andreamartz.js
+++ src/_data/members/members/andreamartz.js
@@ -6,25 +6,29 @@ module.exports = {
   // you can override that data here, as well as provide some additional account links below
   //
   // name - if not defined here, will default to your Name on github if defined, if not, then your username
-  // name: 'Your Name',
+  name: 'Andrea Martz',
   //
   // mainUrl: the url your name links to on the members page. defaults to your github profile
-  // mainUrl: 'https://virtualcoffee.io',
+  // mainUrl: 'https://andreamartz.dev/',
   //
   // bio - accepts markdown.
-  // bio: `This is _my_ **bio** and [here is a link](https://virtualcoffee.io)`,
+  bio: `Full-stack dev | Avid learner | Passionate about education and equity & using tech to make it happen`,
   //
   // can take  one of each type except website - you can add as many `website` accounts as you wish
   accounts: [
-    // { type: 'linkedin', username: 'yourlinkedinUserName' },
+    { type: 'linkedin', username: 'andreamartz' },
     // { type: 'dev', username: 'yourUserName' },
     // { type: 'codenewbie', username: 'yourUserName' },
-    // { type: 'twitter', username: 'yourUserName' },
+    { type: 'twitter', username: 'amartzcoder' },
     // { type: 'twitch', username: 'yourUserName' },
     // { type: 'youtube', channelId: 'yourChannelId' }, // or { type: 'youtube', customUrl: 'https://www.youtube.com/c/yourCustomUrl' }
     // { type: 'polywork', username: 'yourUserName' },
     // { type: 'medium', username: 'yourUserName' },
     // { type: 'hashnode', username: 'yourUserName' },
-    // { type: 'website', url: 'https://virtualcoffee.io', title: 'Title of link' },
+    {
+      type: 'website',
+      url: 'https://andreamartz.dev/',
+      title: "Andrea's Portfolio",
+    },
   ],
 };
