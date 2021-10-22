e38bf9aa2cfc8e73f7eb554b4a0fa48f53d93b96
Matthew <mtfoley.mae@gmail.com>
2021-10-22 10:49:21 -0400

    Create mtfoley.js (#450)

---
+++ src/_data/members/members/mtfoley.js
@@ -0,0 +1,30 @@
+module.exports = {
+	// github username: required
+	github: 'mtfoley',
+	//
+	// everything below here is optional. by default, we pull most profile data from your github profile.
+	// you can override that data here, as well as provide some additional account links below
+	//
+	// name - if not defined here, will default to your Name on github if defined, if not, then your username
+	name: 'Matthew Foley',
+	//
+	// mainUrl: the url your name links to on the members page. defaults to your github profile
+	// mainUrl: 'https://virtualcoffee.io',
+	//
+	// bio - accepts markdown.
+	// bio: `This is _my_ **bio** and [here is a link](https://virtualcoffee.io)`,
+	//
+	// can take  one of each type except website - you can add as many `website` accounts as you wish
+	accounts: [
+		// { type: 'linkedin', username: 'yourlinkedinUserName' },
+		{ type: 'dev', username: 'mtfoley' },
+		// { type: 'codenewbie', username: 'yourUserName' },
+		{ type: 'twitter', username: 'MatthewTFoley' },
+		// { type: 'twitch', username: 'yourUserName' },
+		// { type: 'youtube', channelId: 'yourChannelId' }, // or { type: 'youtube', customUrl: 'https://www.youtube.com/c/yourCustomUrl' }
+		{ type: 'polywork', username: 'mtfoley' },
+		// { type: 'medium', username: 'yourUserName' },
+		// { type: 'hashnode', username: 'yourUserName' },
+		// { type: 'website', url: 'https://virtualcoffee.io', title: 'Title of link' },
+	],
+};
