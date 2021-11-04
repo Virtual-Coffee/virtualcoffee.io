1e2e561ceba224525342f6e7fe2ff83ad08cc52a
Meg Gutshall <meghan.gutshall@gmail.com>
2021-11-03 20:10:30 -0400

    Reword membership profile template for clarity (#463)
    * Reword _EXAMPLE.js for clarity
    
    * Copy comments from example to comments in other member files
    
    * Fix Todd Libby YT channel link
    
    * Add links for Mike Rogers member profile

--- src/_data/members/members/mikerogers0.js
+++ src/_data/members/members/mikerogers0.js
@@ -1,30 +1,33 @@
 module.exports = {
-	// github username: required
+	// GitHub username (required)
 	github: 'mikerogers0',
 	//
-	// everything below here is optional. by default, we pull most profile data from your github profile.
-	// you can override that data here, as well as provide some additional account links below
+	// Everything below here is optional. By default, we pull most profile data from your GitHub profile. You can override that data here, as well as provide some additional account links below.
 	//
-	// name - if not defined here, will default to your Name on github if defined, if not, then your username
+	// Name - If not defined here, it will default to your display name on GitHub. If that's not defined, then your GitHub username.
 	// name: 'Your Name',
 	//
-	// mainUrl: the url your name links to on the members page. defaults to your github profile
+	// Main URL - If not defined here, it will default to the website displayed on your GitHub profile. If that's not defined, then a link to your GitHub profile will be displayed.
 	// mainUrl: 'https://virtualcoffee.io',
 	//
-	// bio - accepts markdown.
+	// Bio - Accepts [markdown](https://spec.commonmark.org/0.30/). Please keep your bio to a reasonable length. Refer to our [members page](https://virtualcoffee.io/members/) for examples.
 	// bio: `This is _my_ **bio** and [here is a link](https://virtualcoffee.io)`,
 	//
-	// can take  one of each type except website - you can add as many `website` accounts as you wish
+	// Links - You can add one of each type, except website - you can add as many `website` accounts as you wish.
 	accounts: [
 		// { type: 'linkedin', username: 'yourlinkedinUserName' },
-		// { type: 'dev', username: 'yourUserName' },
+		{ type: 'dev', username: 'mikerogers0' },
 		// { type: 'codenewbie', username: 'yourUserName' },
-		// { type: 'twitter', username: 'yourUserName' },
+		{ type: 'twitter', username: 'MikeRogers0' },
 		// { type: 'twitch', username: 'yourUserName' },
-		// { type: 'youtube', channelId: 'yourChannelId' }, // or { type: 'youtube', customUrl: 'https://www.youtube.com/c/yourCustomUrl' }
+		{ type: 'youtube', customUrl: 'https://www.youtube.com/c/MikeRogers0' },
 		// { type: 'polywork', username: 'yourUserName' },
 		// { type: 'medium', username: 'yourUserName' },
 		// { type: 'hashnode', username: 'yourUserName' },
-		// { type: 'website', url: 'https://virtualcoffee.io', title: 'Title of link' },
+		{
+			type: 'website',
+			url: 'https://mikerogers.io/',
+			title: 'Ruby Developer | Mike Rogers',
+		},
 	],
 };
--- src/_data/members/members/toddlibby.js
+++ src/_data/members/members/toddlibby.js
@@ -1,28 +1,27 @@
 module.exports = {
-	// github username: required
+	// GitHub username (required)
 	github: 'colabottles',
 	//
-	// everything below here is optional. by default, we pull most profile data from your github profile.
-	// you can override that data here, as well as provide some additional account links below
+	// Everything below here is optional. By default, we pull most profile data from your GitHub profile. You can override that data here, as well as provide some additional account links below.
 	//
-	// name - if not defined here, will default to your Name on github if defined, if not, then your username
+	// Name - If not defined here, it will default to your display name on GitHub. If that's not defined, then your GitHub username.
 	// name: 'Your Name',
 	//
-	// mainUrl: the url your name links to on the members page. defaults to your github profile
+	// Main URL - If not defined here, it will default to the website displayed on your GitHub profile. If that's not defined, then a link to your GitHub profile will be displayed.
 	// mainUrl: 'https://virtualcoffee.io',
 	//
-	// bio - accepts markdown. Please keep your bio to a reasonable length, refer to
+	// Bio - Accepts [markdown](https://spec.commonmark.org/0.30/). Please keep your bio to a reasonable length. Refer to our [members page](https://virtualcoffee.io/members/) for examples. Please keep your bio to a reasonable length, refer to
 	// [members page](https://virtualcoffee.io/members/) for examples
 	// bio: `This is _my_ **bio** and [here is a link](https://virtualcoffee.io)`,
 	//
-	// can take  one of each type except website - you can add as many `website` accounts as you wish
+	// Links - You can add one of each type, except website - you can add as many `website` accounts as you wish.
 	accounts: [
 		{ type: 'linkedin', username: 'todd-libby' },
 		{ type: 'dev', username: 'colabottles' },
 		{ type: 'codenewbie', username: 'colabottles' },
 		{ type: 'twitter', username: 'toddlibby' },
 		{ type: 'twitch', username: 'toddlibby' },
-		{ type: 'youtube', channelId: 'FrontEndNerdery' }, // or { type: 'youtube', customUrl: 'https://www.youtube.com/c/FrontEndNerdery' }
+		{ type: 'youtube', customUrl: 'https://www.youtube.com/c/FrontEndNerdery' },
 		{ type: 'polywork', username: 'colabottles' },
 		// { type: 'medium', username: 'yourUserName' },
 		// { type: 'hashnode', username: 'yourUserName' },
