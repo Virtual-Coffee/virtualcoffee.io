738cf0449e8bd8b80319347f1dbe57298f4a81ea
Dan Ott <dan@dtott.com>
2022-03-11 11:08:48 -0500

    Add Nav and Footer

--- app/root.jsx
+++ app/root.jsx
@@ -6,12 +6,21 @@ import {
 	Scripts,
 	ScrollRestoration,
 } from 'remix';
+import Nav from './components/Nav';
+import styles from './styles/main.css';
+import { useLocation } from 'react-router-dom';
+
+export const links = () => {
+	return [{ rel: 'stylesheet', href: styles }];
+};
 
 export function meta() {
 	return { title: 'New Remix App' };
 }
 
 export default function App() {
+	const location = useLocation();
+
 	return (
 		<html lang="en">
 			<head>
@@ -20,8 +29,55 @@ export default function App() {
 				<Meta />
 				<Links />
 			</head>
-			<body>
+			<body className={location.pathname === '/' && 'vc-home'}>
+				<a
+					href="#maincontent"
+					className={`text-assistive display-at-top-on-focus`}
+				>
+					Skip to main content.
+				</a>
+				<Nav />
 				<Outlet />
+
+				<footer
+					className="
+				py-5
+				text-center text-muted
+				bg-white
+				border-top border-secondary
+				main-footer
+			"
+				>
+					<ul>
+						<li>&copy; {new Date().getFullYear()} Virtual Coffee</li>
+						<li>
+							<a href="mailto:hello@virtualcoffee.io">Contact Us</a>
+						</li>
+						{/* <li>
+					<a
+						href="https://github.com/Virtual-Coffee/virtualcoffee.io/blob/main/{{ page.inputPath }}"
+						>Edit this page</a
+					>
+				</li> */}
+						<li>
+							<a href="/uses">Uses</a>
+						</li>
+						<li className="py-0">
+							<a
+								href="https://www.netlify.com"
+								target="_blank"
+								rel="noopener noreferrer"
+							>
+								<img
+									src="https://www.netlify.com/img/global/badges/netlify-dark.svg"
+									alt="Deploys by Netlify"
+									loading="lazy"
+								/>
+							</a>
+						</li>
+					</ul>
+				</footer>
+
 				<ScrollRestoration />
 				<Scripts />
 				<LiveReload />
