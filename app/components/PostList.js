835aabb967bf471af5c2776c7439a970a4efae78
Dan Ott <dan@dtott.com>
2022-03-15 09:37:33 -0400

    allow for no links

--- app/components/PostList.js
+++ app/components/PostList.js
@@ -3,16 +3,22 @@ import { Link } from 'remix';
 export default function PostList({ items }) {
 	return (
 		<ul className="postlist">
-			{items.map((item) => (
-				<li className="postlist-item" key={item.title}>
-					<Link className="postlist-title" to={item.url}>
-						{item.title}
-					</Link>
-					{item.description && (
-						<p className="postlist-description">{item.description}</p>
-					)}
-				</li>
-			))}
+			{items.map((item) => {
+				return (
+					<li className="postlist-item" key={item.title}>
+						{item.url ? (
+							<Link className="postlist-title" to={item.url}>
+								{item.title}
+							</Link>
+						) : (
+							item.title
+						)}
+						{item.description && (
+							<p className="postlist-description">{item.description}</p>
+						)}
+					</li>
+				);
+			})}
 		</ul>
 	);
 }
