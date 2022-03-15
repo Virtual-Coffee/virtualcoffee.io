97fc5a8c8b218b9fe47909fd11597219586e1abd
Dan Ott <dan@dtott.com>
2022-03-15 09:21:54 -0400

    Add podcast index

--- app/routes/podcast/index.jsx
+++ app/routes/podcast/index.jsx
@@ -1,15 +1,90 @@
-import { useLoaderData, Link } from 'remix';
-import { loadMdx } from '~/util/loadMdx.server.js';
+import { useEffect } from 'react';
+import { useLoaderData, Link, json } from 'remix';
+import { Default } from '~/components/layouts/Default';
+import PodcastSubscribe from '~/components/PodcastSubscribe';
+import PostList from '~/components/PostList';
+import { getEpisodes, getPlayerSrc } from '~/data/podcast';
+import UndrawWalkInTheCity from '~/svg/UndrawWalkInTheCity';
 
-export const loader = () => {
-	const episodes = loadMdx();
+export const loader = async () => {
+	const podcastEpisodes = await getEpisodes({ limit: 100 });
 
-	return episodes;
+	const latestEpisode = podcastEpisodes[0];
+	const latestEpisodePlayerSrc = getPlayerSrc({
+		id: latestEpisode.podcastBuzzsproutId,
+	});
+
+	return json({ podcastEpisodes, latestEpisode, latestEpisodePlayerSrc });
 };
 
 export default function PodcastsIndex() {
-	const episodes = useLoaderData();
-	console.log(episodes);
+	const { podcastEpisodes, latestEpisode, latestEpisodePlayerSrc } =
+		useLoaderData();
 
-	return <div>hello!</div>;
+	useEffect(() => {
+		if (latestEpisodePlayerSrc && document) {
+			const script = document.createElement('script');
+
+			script.src = latestEpisodePlayerSrc;
+			script.async = true;
+
+			document.body.appendChild(script);
+
+			return () => {
+				document.body.removeChild(script);
+			};
+		}
+	}, [latestEpisodePlayerSrc]);
+
+	return (
+		<Default heroHeader="Virtual Coffee Podcast" Hero={UndrawWalkInTheCity}>
+			<div className="container bodycopy py-5">
+				<p className="lead">
+					This is the Virtual Coffee Podcast, where we interview members of the
+					community to learn more about their stories, who they are, how they
+					found Virtual Coffee, and how that influences their lives as
+					developers.
+				</p>
+
+				<hr />
+
+				<PodcastSubscribe />
+
+				<hr />
+
+				<div className="text-muted">Latest Episode:</div>
+				<h3>
+					<Link to={`/podcast/${latestEpisode.slug}`}>
+						{latestEpisode.title}
+					</Link>
+				</h3>
+
+				<div className="mt-4 podcastplayer">
+					{latestEpisodePlayerSrc && (
+						<>
+							<div
+								id={`buzzsprout-player-${latestEpisode.podcastBuzzsproutId}`}
+							></div>
+						</>
+					)}
+				</div>
+			</div>
+
+			<div className="bg-light py-5">
+				<div className="container-xl">
+					<h2 className="display-5">All Episodes:</h2>
+
+					<PostList
+						items={podcastEpisodes.map(
+							({ title, metaDescription: description, slug }) => ({
+								title,
+								description,
+								url: `/podcast/${slug}`,
+							}),
+						)}
+					/>
+				</div>
+			</div>
+		</Default>
+	);
 }
