2af1e2a63686880af7f275479cd5f7fd7e8a640d
Dan Ott <dan@dtott.com>
2022-03-23 14:14:44 -0400

    Add favicon meta tags

--- app/root.jsx
+++ app/root.jsx
@@ -26,6 +26,36 @@ export default function App() {
 			<head>
 				<meta charSet="utf-8" />
 				<meta name="viewport" content="width=device-width,initial-scale=1" />
+				<link
+					rel="apple-touch-icon"
+					sizes="180x180"
+					href="/assets/favicon/apple-touch-icon.png"
+				/>
+				<link
+					rel="icon"
+					type="image/png"
+					sizes="32x32"
+					href="/assets/favicon/favicon-32x32.png"
+				/>
+				<link
+					rel="icon"
+					type="image/png"
+					sizes="16x16"
+					href="/assets/favicon/favicon-16x16.png"
+				/>
+				<link rel="manifest" href="/assets/favicon/site.webmanifest" />
+				<link
+					rel="mask-icon"
+					href="/assets/favicon/safari-pinned-tab.svg"
+					color="#d9376e"
+				/>
+				<link rel="shortcut icon" href="/assets/favicon/favicon.ico" />
+				<meta name="msapplication-TileColor" content="#d9376e" />
+				<meta
+					name="msapplication-config"
+					content="/assets/favicon/browserconfig.xml"
+				/>
+				<meta name="theme-color" content="#ffffff" />
 				<Meta />
 				<Links />
 			</head>
