37ef33e04fdabc4a064d84634e9814fc2b6b1fd3
Dan Ott <dan@dtott.com>
2022-03-28 08:26:32 -0400

    check to not redirect on home page

--- app/util/http.js
+++ app/util/http.js
@@ -2,7 +2,7 @@ import { redirect } from 'remix';
 
 export function removeTrailingSlash(request) {
 	const url = new URL(request.url);
-	if (url.pathname.endsWith('/')) {
+	if (url.pathname.endsWith('/') && url.pathname !== '/') {
 		throw redirect(url.pathname.slice(0, -1), {
 			status: 308,
 		});
