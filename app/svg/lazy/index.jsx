a6df4366aa7474240e9eaf3ad2e74369ded9459f
Dan Ott <dan@dtott.com>
2022-04-11 11:28:49 -0400

    set aspect ratio on image directly

--- app/svg/lazy/index.jsx
+++ app/svg/lazy/index.jsx
@@ -1,45 +1,31 @@
-import React, { Suspense, useMemo, useEffect, useState } from 'react';
-
 const svgs = {
 	UndrawCelebration: {
-		Icon: React.lazy(() => import(`~/svg/UndrawCelebration`)),
 		aspectRatio: '949.8 / 661.52',
 	},
 
 	UndrawWalkInTheCity: {
-		Icon: React.lazy(() => import(`~/svg/UndrawWalkInTheCity`)),
 		aspectRatio: '955 / 680.5',
 	},
 
 	UndrawConferenceCall: {
-		Icon: React.lazy(() => import(`~/svg/UndrawConferenceCall`)),
 		aspectRatio: '817.21528 / 523.62072',
 	},
 
 	UndrawFolder: {
-		Icon: React.lazy(() => import(`~/svg/UndrawFolder`)),
 		aspectRatio: '929.54484 / 793.07015',
 	},
 
 	UndrawArrived: {
-		Icon: React.lazy(() => import(`~/svg/UndrawArrived`)),
 		aspectRatio: '1034 / 823.8434',
 	},
 
 	UndrawGoodTeam: {
-		Icon: React.lazy(() => import(`~/svg/UndrawGoodTeam`)),
 		aspectRatio: '1115.91385 / 832.5636',
 	},
 };
 
 export default function LazyIcon({ filename, className }) {
-	const [isClient, setIsClient] = useState(false);
-	const { Icon, aspectRatio } = svgs[filename];
-	useEffect(() => {
-		if (typeof document !== 'undefined') {
-			setIsClient(true);
-		}
-	}, []);
+	const { aspectRatio } = svgs[filename];
 
 	return (
 		<div
@@ -48,7 +34,11 @@ export default function LazyIcon({ filename, className }) {
 			}}
 			className={className}
 		>
-			<img src={`/assets/svg/${filename}.svg`} loading="lazy" />
+			<img
+				style={{ aspectRatio }}
+				src={`/assets/svg/${filename}.svg`}
+				loading="lazy"
+			/>
 		</div>
 	);
 }
