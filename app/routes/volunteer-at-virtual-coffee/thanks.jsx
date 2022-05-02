d4ba6983820798c4fbed59bc7c6d8fade98dd9db
Dan Ott <dan@dtott.com>
2022-05-02 14:19:41 -0400

    Feature/leadership resources (#562)
    * update dependencies
    
    * Fix issue with mdx files not updating header
    
    * Add new svgs
    
    * add coffee table groups file
    
    * Add paths to leadership
    
    * Update coffee-table-groups.mdx
    
    * Update paths-to-leadership.mdx
    
    * small updates to path roles and duties
    
    * move get involved files into subdirectory
    
    * better handle deeply-nested files
    
    * Add volunteer form
    
    * Update order/title
    
    * Update order / add link to form
    
    * Add lunch and learns page
    
    * Add coffee table group form
    
    * debugging url
    
    * continue debugging thing
    
    * Update index.mdx
    
    * (╯°□°）╯︵ ┻━┻
    
    * Update lunch-and-learns.mdx
    
    * Update paths-to-leadership.mdx
    
    * updated descriptions
    
    * Update paths-to-leadership.mdx
    
    * remove extra TOC
    
    * formatting
    
    * Some more formatting
    
    * Re-org with departments
    
    * attempt to create build urls at build time
    
    * clean up
    
    * Add ability to pre-fill form
    
    * Add in-context links to form
    
    * No more default text
    
    Co-authored-by: BekahHW <34313413+BekahHW@users.noreply.github.com>
    Co-authored-by: Kirk Shillingford <tkshillinz@icloud.com>

---
+++ app/routes/volunteer-at-virtual-coffee/thanks.jsx
@@ -0,0 +1,26 @@
+import { json } from 'remix';
+import DefaultLayout from '~/components/layouts/DefaultLayout';
+
+export async function loader() {
+	return json({
+		meta: {
+			title: 'Volunteer Application Received!',
+			description: `Thank you so much for your willingness to help at VC!`,
+		},
+	});
+}
+
+export function meta({ data: { meta } }) {
+	return meta;
+}
+
+export default function Thanks() {
+	return (
+		<DefaultLayout simple Hero="UndrawPowerful">
+			<div class="lead mb-5">
+				<h2>Thank you so much for your willingness to help at VC!</h2>
+				<p>One of our team members will get back to you shortly.</p>
+			</div>
+		</DefaultLayout>
+	);
+}
