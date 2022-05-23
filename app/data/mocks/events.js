1f6e852fcad73288981d2d96de9b41d5336a8ef2
Joe Karow <58997957+JoeKarow@users.noreply.github.com>
2022-05-23 14:04:01 -0400

    Add to Calendar button on event cards (#574)
    * install calendar-links package
    
    * add 'Add to Calendar' button to event card
    
    * Move ICS generation to data & mock loaders
    
    * tweak styles of button
    
    * remove shadow for btn-link
    
    * Add description
    
    * add desc to event mock
    
    * adjust font size and layout
    
    Co-authored-by: Dan Ott <dan@dtott.com>

--- app/data/mocks/events.js
+++ app/data/mocks/events.js
@@ -1,11 +1,18 @@
 import { faker } from '@faker-js/faker';
 import { DateTime } from 'luxon';
+import { ics } from 'calendar-link';
 
 export function createEventsData({ limit = 15, rangeStart, rangeEnd }) {
 	const dates = faker.date.betweens(rangeStart, rangeEnd, limit);
 
 	return dates.map((date) => {
 		const startDate = DateTime.fromJSDate(date);
+		const calendarLink = ics({
+			title: faker.lorem.sentence(7),
+			start: startDate.toString(),
+			end: startDate.plus({ hours: 1 }),
+			description: faker.lorem.paragraph(),
+		});
 
 		return {
 			id: faker.datatype.uuid(),
@@ -13,6 +20,7 @@ export function createEventsData({ limit = 15, rangeStart, rangeEnd }) {
 			startDateLocalized: startDate.toString(),
 			endDateLocalized: startDate.plus({ hours: 1 }),
 			eventCalendarDescription: `<p>${faker.lorem.paragraph()}</p>`,
+			eventCalendarLink: calendarLink,
 		};
 	});
 }
