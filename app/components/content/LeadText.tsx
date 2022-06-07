6cbfa014cd448c99130329ad8470d0fa1ef7c54d
Dan Ott <dan@dtott.com>
2022-06-07 13:43:05 -0400

    set return type as JSX.Element

--- app/components/content/LeadText.tsx
+++ app/components/content/LeadText.tsx
@@ -2,6 +2,6 @@ type LeadTextProps = {
 	children: React.ReactNode;
 };
 
-export default function LeadText({ children }: LeadTextProps) {
+export default function LeadText({ children }: LeadTextProps): JSX.Element {
 	return <div className="lead">{children}</div>;
 }
