b7bf6f599fdfe209d40d38cb1c57cb48d2a3e5c8
Dan Ott <dan@dtott.com>
2022-06-21 15:11:08 -0400

    import sponsor type

--- app/data/sponsors.ts
+++ app/data/sponsors.ts
@@ -1,8 +1,7 @@
+import type { SponsorsResponse } from 'netlify/functions/data-sponsors';
 import { qualifiedUrl } from '~/util/url.server';
 
-type SponsorResponse = any;
-
-export default async function getSponsors(): Promise<SponsorResponse> {
+export default async function getSponsors(): Promise<SponsorsResponse> {
 	const response = await fetch(
 		qualifiedUrl(`/.netlify/builders/data-sponsors`),
 	).then((res) => res.json());
