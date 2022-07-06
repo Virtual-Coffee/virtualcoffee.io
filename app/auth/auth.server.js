bfa42a54c85307efa647cb37870038d1af44e732
Dan Ott <dan@dtott.com>
2022-07-06 10:52:01 -0400

    testing error states

--- app/auth/auth.server.js
+++ app/auth/auth.server.js
@@ -6,7 +6,7 @@ import Api from '~/api/cms.server';
 // Create an instance of the authenticator, pass a generic with what
 // strategies will return and will store in the session
 export let authenticator = new Authenticator(sessionStorage, {
-	throwOnError: true,
+	// throwOnError: true,
 });
 
 // Tell the Authenticator to use the form strategy
@@ -17,16 +17,16 @@ authenticator.use(
 		let email = form.get('email');
 		let password = form.get('password');
 
-		try {
-			let user = await api.login(email, password);
-			// the type of this user must match the type you pass to the Authenticator
-			// the strategy will automatically inherit the type if you instantiate
-			// directly inside the `use` method
-			console.log({ user });
-			return user;
-		} catch (error) {
-			throw new AuthorizationError(error.message, error.data?.errors);
-		}
+		// try {
+		let user = await api.login(email, password);
+		// the type of this user must match the type you pass to the Authenticator
+		// the strategy will automatically inherit the type if you instantiate
+		// directly inside the `use` method
+		console.log({ user });
+		return user;
+		// } catch (error) {
+		// 	throw new AuthorizationError(error.message, error.data?.errors);
+		// }
 	}),
 	// each strategy has a name and can be changed to use another one
 	// same strategy multiple times, especially useful for the OAuth2 strategy.
--- app/routes/auth/login.jsx
+++ app/routes/auth/login.jsx
@@ -12,6 +12,7 @@ import { AuthorizationError } from 'remix-auth';
 function LogInForm({ error }) {
 	return (
 		<Form method="post">
+			<div>yo</div>
 			{error && (
 				<div className="alert alert-danger">
 					<p>{error}</p>
@@ -41,6 +42,7 @@ export function CatchBoundary() {
 // names we are going to use in the strategy
 export default function Screen() {
 	const actionData = useActionData();
+	console.log({ actionData });
 
 	return <LogInForm error={actionData?.error} />;
 }
@@ -48,34 +50,41 @@ export default function Screen() {
 // Second, we need to export an action function, here we will use the
 // `authenticator.authenticate method`
 export let action = async ({ request }) => {
+	await authenticator.isAuthenticated(request, {
+		successRedirect: '/membership/dashboard',
+	});
+	let session = await getSession(request.headers.get('cookie'));
+	let error = session.get(authenticator.sessionErrorKey);
+	return json({ error });
+
 	// we call the method with the name of the strategy we want to use and the
 	// request object, optionally we pass an object with the URLs we want the user
 	// to be redirected to after a success or a failure
-	try {
-		return await authenticator.authenticate('user-pass', request, {
-			successRedirect: '/membership/dashboard',
-		});
-	} catch (error) {
-		// Because redirects work by throwing a Response, you need to check if the
-		// caught error is a response and return it or throw it again
-		if (error instanceof Response) {
-			console.log('is response', error);
-			return error;
-		}
-		if (error instanceof AuthorizationError) {
-			console.log('is auth error');
-			console.log(error);
-			console.log({
-				message: error.message,
-				data: error.data,
-			});
-			return json({ error: error.message });
-			// here the error is related to the authentication process
-		}
-		// here the error is a generic error that another reason may throw
-		console.log('is generic error');
-		return json({ error: 'There was a server error.' });
-	}
+	// try {
+	// 	return await authenticator.authenticate('user-pass', request, {
+	// 		successRedirect: '/membership/dashboard',
+	// 	});
+	// } catch (error) {
+	// 	// Because redirects work by throwing a Response, you need to check if the
+	// 	// caught error is a response and return it or throw it again
+	// 	if (error instanceof Response) {
+	// 		console.log('is response', error);
+	// 		return error;
+	// 	}
+	// 	if (error instanceof AuthorizationError) {
+	// 		console.log('is auth error');
+	// 		console.log(error);
+	// 		console.log({
+	// 			message: error.message,
+	// 			data: error.data,
+	// 		});
+	// 		return json({ error: error.message });
+	// 		// here the error is related to the authentication process
+	// 	}
+	// 	// here the error is a generic error that another reason may throw
+	// 	console.log('is generic error');
+	// 	return json({ error: 'There was a server error.' });
+	// }
 };
 
 // Finally, we can export a loader function where we check if the user is
