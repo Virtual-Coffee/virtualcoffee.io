a3b9e8a03afec97a1c77bb7cc6a8951686bdde6c
Dan Ott <dan@dtott.com>
2022-07-07 15:51:16 -0400

    add route protection back in to dashboard

--- app/routes/membership/dashboard.tsx
+++ app/routes/membership/dashboard.tsx
@@ -1,6 +1,11 @@
 import { json } from '@remix-run/node';
+import type { LoaderFunction } from '@remix-run/node';
 import { Link } from '@remix-run/react';
 import { authenticate } from '~/auth/auth.server';
+
+export const loader: LoaderFunction = async ({ request }) => {
+	await authenticate(request);
+};
 
 export default function Page() {
 	return <div>Dashboard</div>;
