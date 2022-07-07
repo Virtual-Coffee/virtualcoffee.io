f1f66754bc531fd741d0e6d042d748d1c1080ef5
Dan Ott <dan@dtott.com>
2022-07-07 15:47:28 -0400

    move auth into membership route

---
+++ app/routes/membership.tsx
@@ -0,0 +1,29 @@
+import { json } from '@remix-run/node';
+import type { LoaderFunction } from '@remix-run/node';
+import { Link } from '@remix-run/react';
+import { authenticate } from '~/auth/auth.server';
+import { Outlet } from '@remix-run/react';
+import DefaultLayout from '~/components/layouts/DefaultLayout';
+
+export const loader: LoaderFunction = async ({ request }) => {
+	let user = await authenticate(request);
+	if (user) {
+		// console.log({ user: user?.authenticate?.user });
+		console.log({ user });
+	} else {
+		console.log('no user');
+	}
+
+	return json({ x: 1 });
+};
+
+export default function Page() {
+	return (
+		<DefaultLayout simple showHero={false}>
+			<div>
+				<Link to="/auth/logout">Log Out</Link>
+			</div>
+			<Outlet />
+		</DefaultLayout>
+	);
+}
--- app/routes/membership/dashboard.jsx
+++ app/routes/membership/dashboard.jsx
@@ -2,22 +2,6 @@ import { json } from '@remix-run/node';
 import { Link } from '@remix-run/react';
 import { authenticate } from '~/auth/auth.server';
 
-export async function loader({ request }) {
-	let user = await authenticate(request);
-	if (user) {
-		// console.log({ user: user?.authenticate?.user });
-		console.log({ user });
-	} else {
-		console.log('no user');
-	}
-
-	return json({ x: 1 });
-}
-
 export default function Page() {
-	return (
-		<div>
-			<Link to="/auth/logout">Log Out</Link>
-		</div>
-	);
+	return <div>Dashboard</div>;
 }
