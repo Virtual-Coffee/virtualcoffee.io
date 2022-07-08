400baa0192157412e969d5b85d6c638b5996ef38
Retno Ayu Adiati <retno_ayu@hotmail.com>
2022-07-08 11:48:42 +0200

    Add July 2022 newsletter

---
+++ app/routes/newsletter/issues/2022-07.jsx
@@ -0,0 +1,274 @@
+export const handle = {
+	meta: {
+		title: 'Virtual Coffee Newsletter, July 2022',
+		description: 'Virtual Coffee is Building in Public in July 💝',
+	},
+	date: '2022-07-01',
+	listTitle: 'July 2022',
+};
+
+export const meta = () => {
+	return handle.meta;
+};
+
+export default function Issue() {
+	return (
+		<>
+			<h2>Hey friends!</h2>
+
+			<p className="lead">
+				It's time to kick it into high gear this summer with Demoing and
+				Building in Public!
+			</p>
+
+			<hr />
+
+			<h2>💞 Community Kindness</h2>
+			<p>
+				<em>Spotlighting some of the kindness happening in our community.</em>
+			</p>
+			<blockquote className="blockquote">
+				<p className="mb-0">
+					"Grateful to have this community full of amazing knowledgeable people.
+					Highlight of my day is seeing the connectivity and helpfulness that
+					this community displays."
+				</p>
+				<footer className="blockquote-footer">Jeff</footer>
+			</blockquote>
+			<blockquote className="blockquote">
+				<p className="mb-0">
+					"I'm super grateful for my VC family for all the support that I
+					received, especially these last few weeks when I needed it the most!"
+				</p>
+				<footer className="blockquote-footer">Ayu</footer>
+			</blockquote>
+			<blockquote className="blockquote">
+				<p className="mb-0">
+					"I'm grateful for my family, this awesome community, and my first week
+					at work was smooth."
+				</p>
+				<footer className="blockquote-footer">Reda</footer>
+			</blockquote>
+			<blockquote className="blockquote">
+				<p className="mb-0">
+					"I am grateful to have found an inclusive and welcoming tech community
+					with members willing to lend me their expertise and insight :D"
+				</p>
+				<footer className="blockquote-footer">Dominic</footer>
+			</blockquote>
+
+			<hr />
+
+			<h2 className="my-5">📆 What's happening at Virtual Coffee</h2>
+
+			<h3 className="font-italic">June Recap</h3>
+			<p>
+				Monthly challenge -&gt;{' '}
+				<a href="https://virtualcoffee.io/monthlychallenges/june-2022">
+					All About Feedback!
+				</a>
+			</p>
+			<p>
+				We spent June trying to give meaningful feedback in the community, and
+				had a lot of discussions about what it means to give and receive
+				critical feedback!
+			</p>
+
+			<h3 className="mb-3 font-italic">Volunteering at VC</h3>
+			<p>
+				Over the last few months we've been trying to improve our documentation
+				here at VC. The team continues to work hard prepping for the next phase
+				of Virtual Coffee. We have a lot of great things planned and we can't
+				wait to open up membership again to an even better, more supportive,
+				more inviting community. We are still so grateful to all of you on this
+				journey with us and eagerly looking forward to opening up membership
+				again! If you're interested in joining the volunteer team, check out
+				some the roles{' '}
+				<a href="https://virtualcoffee.io/resources/virtual-coffee/get-involved/paths-to-leadership">
+					here
+				</a>
+				!
+			</p>
+
+			<h3 className="mb-3 font-italic">July Happenings</h3>
+			<h4 className="mt-4">
+				💡 Monthly Theme &amp; Challenge:{' '}
+				<a href="https://virtualcoffee.io/monthlychallenges/july-2022">
+					Demoing and Building in Public
+				</a>
+				!
+			</h4>
+			<h5>
+				We're challenging you to do some amazing work and share it with the
+				world.
+			</h5>
+			<p>
+				This is the month for sharing your interests with others. Many times, we
+				fail to do what we set out to do and let our goals quietly escape us.
+				But this month, we're getting ahead of that by summoning our entire
+				support squad and sharing our goals, our progress, and our successes.
+			</p>
+			<p>
+				If you've got a project this month, don't keep it to yourself. You can
+				write about it, tweet it, make videos, or just share in friendly forums
+				and spaces, but dont keep your efforts hidden. The entire community is
+				checking in every day!
+			</p>
+			<p>
+				You can check out our{' '}
+				<a href="https://virtualcoffee.io/monthlychallenges">
+					monthly challenge page
+				</a>{' '}
+				for more ideas on how to get started with our empathetic feedback
+				challenge.
+			</p>
+
+			<h4 className="mt-4">☕🪑 Coffee Table Events</h4>
+			<ul>
+				<li>
+					Hosting Github Pages With Github Actions with Jörn Bernhardt — Friday
+					8th at 12:00 PM ET
+				</li>
+				<li>Tech Interview Study Group (Mondays at 4:00 PM ET)</li>
+				<li>
+					Job Seekers Coworking (Tuesdays at 7:00 PM ET and Thursdays at 9:00 AM
+					ET)
+				</li>
+				<li>
+					React/JS Meetup (Wednesday at 1:00 PM ET on the first and third weeks
+					of the month)
+				</li>
+				<li>
+					VC Toastmasters (Every other Monday at 12:00 PM ET — see
+					#content-creation)
+				</li>
+				<li>
+					Indie-startup hackers (Every other Wednesday at 12:00 PM ET - see
+					#indie-startup-hackers)
+				</li>
+			</ul>
+
+			<p>
+				Note: These are the currently scheduled times for these events at the
+				time of this publication. Please check the official VC #announcements
+				Slack channel, or other noted channels, for any updates and links to
+				event rooms.
+			</p>
+
+			<h2>Member Blogpost Highlights</h2>
+			<p>
+				<em>
+					Here's some of the awesome articles our members wrote last month!
+				</em>
+			</p>
+			<ul>
+				<li>
+					<a href="https://dev.to/chad_r_stewart/part-0-bonus-logging-middlewares-and-migrating-image-manipulation-functions-into-a-services-file-33h9">
+						Logging, middlewares and migrating Image Manipulation functions into
+						a Services file — Chad Stewart
+					</a>
+				</li>
+				<li>
+					<a href="https://dev.to/nickytonline/my-eleventy-meetup-talk-3b2p">
+						Consuming the Deepgram API: The GraphQL Edit — Lucia Cerchie
+					</a>
+				</li>
+				<li>
+					<a href="https://brianmeeker.me/2022/06/07/debugging-transaction-issues-in-mariadb/">
+						Debugging Transaction Issues In MariaDB — Brian Meeker
+					</a>
+				</li>
+				<li>
+					<a href="https://hashnode.iamdeveloper.com/a11y-twitter-a-browser-extension-for-making-twitter-more-accessible-17kg">
+						a11y-twitter: a browser extension for making Tweets more accessible
+						— Nick Taylor
+					</a>
+				</li>
+			</ul>
+
+			<div className="card my-5 border-primary">
+				<div className="card-body">
+					<h5 className="card-title text-primary font-italic">Member Wins</h5>
+					<div className="card-text">
+						<blockquote className="blockquote">
+							<p className="mb-0 font-italic">
+								As always, shout-out to all our members celebrating new roles
+								this month!
+							</p>
+						</blockquote>
+						<blockquote className="blockquote">
+							<p className="mb-0">
+								"I did it, I made it to 1 year of coding in the 100DaysOfCode
+								Challenge!"
+							</p>
+							<footer className="blockquote-footer">Drew</footer>
+						</blockquote>
+						<blockquote className="blockquote">
+							<p className="mb-0">"WIN: I'm officially the company CTO!"</p>
+							<footer className="blockquote-footer">Brian</footer>
+						</blockquote>
+						<blockquote className="blockquote">
+							<p className="mb-0">
+								"Finishing up with my MIT certification this week and just got
+								my acceptance into Product Management School!"
+							</p>
+							<footer className="blockquote-footer">Marie</footer>
+						</blockquote>
+						<blockquote className="blockquote">
+							<p className="mb-0">
+								"Today is my first day ever as a salaried employee in tech!"
+							</p>
+							<footer className="blockquote-footer">Stephen</footer>
+						</blockquote>
+					</div>
+				</div>
+			</div>
+
+			<h2>What our members are up to</h2>
+			<ul>
+				<li>
+					Check out some of our resident streamers:{' '}
+					<a href="https://www.twitch.tv/bekahhw">Bekah</a>,{' '}
+					<a href="https://www.twitch.tv/nickytonline">Nick</a>,{' '}
+					<a href="https://www.twitch.tv/jonathanyeong">Jono</a>,{' '}
+					<a href="https://www.twitch.tv/arthurdoler">Arthur</a>, and{' '}
+					<a href="https://www.twitch.tv/iandouglas736">Ian</a>.
+				</li>
+				<li>
+					<a href="https://www.getrevue.co/profile/nickytonline/issues/yet-another-newsletter-lol-issue-31-1002343">
+						Nick Taylor's newsletter
+					</a>{' '}
+					keeps you up to date with the hot topics in tech.
+				</li>
+				<li>
+					<a href="https://www.getrevue.co/profile/luciacerchie">
+						Lucia Cerchie's newsletter
+					</a>{' '}
+					is a breath of fresh air in the tech space.
+				</li>
+			</ul>
+
+			<h3>Open Source Projects</h3>
+			<ul>
+				<li>
+					<a href="https://github.com/Virtual-Coffee/virtualcoffee.io/issues">
+						VirtualCoffee.io
+					</a>{' '}
+					— an 11ty Web App for our community created by Dan Ott
+				</li>
+				<li>
+					<a href="https://github.com/AmyShackles/regex_parser">Regex Parser</a>{' '}
+					— a regular expression parser project by Amy Shackles
+				</li>
+				<li>
+					<a href="https://github.com/drone/drone">DRONE</a> — a Continuous
+					Delivery system built on container technology
+				</li>
+				<li>
+					<a href="https://jesscss.github.io/">JESS CSS</a> — a CSS
+					pre-processor that compiles to Javascript!
+				</li>
+			</ul>
+		</>
+	);
+}
