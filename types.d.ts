9645ee0e84c740e48dacb7786aa654c7f07f4467
Dan Ott <dan@dtott.com>
2022-08-16 15:29:03 -0400

    Add better types to directory-import definition

--- types.d.ts
+++ types.d.ts
@@ -1,30 +1,56 @@
 declare module 'directory-import' {
-	function importDir(parameters: {
+	type ImportDirParams = {
 		/**
 		 * Relative path to directory
+		 * @default './'
 		 */
 		directoryPath?: string;
-		/**
-		 * Import files synchronously, or asynchronously
-		 */
-		importMethod?: string;
+
 		/**
 		 * If false — files in subdirectories will not be imported
+		 * @default true
 		 */
 		includeSubdirectories?: boolean;
 		/**
 		 * Webpack support.
+		 * @default false
 		 */
 		webpack?: boolean;
 		/**
 		 * Indicates how many files to import. 0 - to disable the limit
+		 * @default 0
 		 */
 		limit?: number;
 		/**
 		 * Exclude files paths.
+		 * @default undefined
 		 */
 		exclude?: RegExp;
-	}): any;
+	};
+
+	type WithImportAsync = {
+		/**
+		 * Import files synchronously, or asynchronously
+		 * @default 'sync'
+		 */
+		importMethod?: 'async';
+	};
+
+	type WithImportSync = {
+		/**
+		 * Import files synchronously, or asynchronously
+		 * @default 'sync'
+		 */
+		importMethod?: 'sync';
+	};
+
+	function importDir<T>(
+		parameters: ImportDirParams & WithImportSync,
+	): Record<string, T>;
+
+	function importDir<T>(
+		parameters: ImportDirParams & WithImportAsync,
+	): Promise<Record<string, T>>;
 
 	export = importDir;
 }
