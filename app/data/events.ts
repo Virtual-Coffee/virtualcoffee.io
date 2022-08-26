fa5e6e6adf4626c03f2512b38206bb7e2fe305d9
Dan Ott <dan@dtott.com>
2022-08-26 13:51:13 -0400

    convert back to graphql

--- app/data/events.ts
+++ app/data/events.ts
@@ -1,16 +1,17 @@
-// import { GraphQLClient gql } from 'graphql-request';
+import { GraphQLClient, gql } from 'graphql-request';
 import { DateTime } from 'luxon';
 import { sanitizeHtml } from '~/util/sanitizeCmsData';
 import { ics } from 'calendar-link';
-import axios from 'axios';
 
-// const calendarsQuery = `query getCalendars {
-// 		solspace_calendar {
-// 			calendars {
-// 				handle
-// 			}
-// 		}
-// 	}`;
+const calendarsQuery = gql`
+	query getCalendars {
+		solspace_calendar {
+			calendars {
+				handle
+			}
+		}
+	}
+`;
 
 /**
  * Defining the interface for the SolspaceCalendar object.
@@ -46,7 +47,7 @@ function createEventsQuery(
 	rangeEnd: string,
 	limit: number,
 ) {
-	return `
+	return gql`
 	query getEvents {
 		solspace_calendar {
 			events(rangeStart: "${rangeStart}", rangeEnd: "${rangeEnd}", limit: ${limit}) {
@@ -85,94 +86,42 @@ export async function getEvents({
 		return fakeData.createEventsData({ limit, rangeEnd, rangeStart });
 	}
 
-	// const instance = axios.create({
-	// 	baseURL: `${process.env.CMS_URL}/api`,
-	// });
-
-	// const graphQLClient = new GraphQLClient(`${process.env.CMS_URL}/api`, {
-	// 	headers: {
-	// 		Authorization: `bearer ${process.env.CMS_TOKEN}`,
-	// 	},
-	// });
-
-	console.log('Fetching events', rangeStart, rangeEnd);
+	const graphQLClient = new GraphQLClient(`${process.env.CMS_URL}/api`, {
+		headers: {
+			Authorization: `bearer ${process.env.CMS_TOKEN}`,
+		},
+	});
 
 	try {
-		console.log('requesting calendars');
-		const calendarsResponse = await axios.request<{
-			data: {
-				solspace_calendar: {
-					calendars: Pick<SolspaceCalendar, 'handle'>[];
-				};
-			};
-		}>({
-			method: 'POST',
-			data: `query getCalendars {
-					solspace_calendar {
-						calendars {
-							handle
-						}
-					}
-				}`,
+		const {
+			solspace_calendar: { calendars },
+		} = await graphQLClient.request<{
+			solspace_calendar: { calendars: Pick<SolspaceCalendar, 'handle'>[] };
+		}>(calendarsQuery);
 
-			url: `${process.env.CMS_URL}/api`,
-			timeout: 4000,
-			headers: {
-				Authorization: `bearer ${process.env.CMS_TOKEN}`,
-				'content-type': 'application/graphql',
-			},
-		});
+		const {
+			solspace_calendar: { events },
+		} = await graphQLClient.request<{
+			solspace_calendar: { events: EventsResponse };
+		}>(createEventsQuery(calendars, rangeStart, rangeEnd, limit));
 
-		console.log(
-			createEventsQuery(
-				calendarsResponse.data.data.solspace_calendar.calendars,
-				rangeStart,
-				rangeEnd,
-				limit,
-			),
-		);
-
-		console.log('requesting events');
-		const eventsResponse = await axios.request({
-			method: 'POST',
-			data: createEventsQuery(
-				calendarsResponse.data.data.solspace_calendar.calendars,
-				rangeStart,
-				rangeEnd,
-				limit,
-			),
-			url: `${process.env.CMS_URL}/api`,
-			timeout: 4000,
-			headers: {
-				Authorization: `bearer ${process.env.CMS_TOKEN}`,
-				'content-type': 'application/graphql',
-			},
-		});
-
-		console.log(eventsResponse.data);
-
-		// return response.slice(0, 10);
-		console.log('parsing events');
-		// console.log(eventsResponse);
 		return await Promise.all(
-			eventsResponse.data.data.solspace_calendar.events.map(
-				async (event: SolspaceEventResponse) => {
-					const sanitizedDescription = await sanitizeHtml(
-						event.eventCalendarDescription,
-					);
-					const calendarLink = await ics({
-						title: event.title,
-						start: event.startDateLocalized,
-						end: event.endDateLocalized,
-						description: sanitizedDescription,
-					});
-					return {
-						...event,
-						eventCalendarDescription: sanitizedDescription,
-						eventCalendarLink: calendarLink,
-					};
-				},
-			),
+			events.map(async (event) => {
+				const sanitizedDescription = await sanitizeHtml(
+					event.eventCalendarDescription,
+				);
+				const calendarLink = ics({
+					title: event.title,
+					start: event.startDateLocalized,
+					end: event.endDateLocalized,
+					description: sanitizedDescription,
+				});
+				return {
+					...event,
+					eventCalendarDescription: sanitizedDescription,
+					eventCalendarLink: calendarLink,
+				};
+			}),
 		);
 	} catch (e) {
 		console.error(e);
