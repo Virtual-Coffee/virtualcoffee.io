f7736f945dfb35912121596506cf740bae7ed998
Joe Karow <58997957+JoeKarow@users.noreply.github.com>
2022-08-26 13:53:58 -0400

    fix: 🐛 suppress hydration warnings on dates (#617)
    * Redo changes from prior branch - git merge hell
    
    - fix: convert start/end datetime to UTC on data fetch
    
    Solspace's startDate/endDate don't actually return UTC. It returns
    the date in EST, just formatted as if it were UTC.
    startDateLocalized/endDateLocalized will give the date formatted
    correctly as EST.
    
    - implement DateTime component to prevent 'zone flash'
    
    Initially, the page would show the UTC time/date and then rerender to
    local time.
    Created a new component that will only render after the conversion
    has been completed.
    
    * add luxon types
    
    * replace DateTime component with suppressHydrationWarning on <time> tag
    
    * revert additional fields for UTC
    
    * convert function to typescript
    
    * tighten up util types
    
    * formatting
    
    * convert event mocks to ts
    
    * adjust typing on calendars
    
    * convert back to graphql
    
    Co-authored-by: Dan Ott <dan@dtott.com>

--- app/data/mocks/events.ts
+++ app/data/mocks/events.ts
@@ -2,23 +2,31 @@ import { faker } from '@faker-js/faker';
 import { DateTime } from 'luxon';
 import { ics } from 'calendar-link';
 
-export function createEventsData({ limit = 15, rangeStart, rangeEnd }) {
+export function createEventsData({
+	limit = 15,
+	rangeStart,
+	rangeEnd,
+}: {
+	limit: number;
+	rangeStart: string;
+	rangeEnd: string;
+}) {
 	const dates = faker.date.betweens(rangeStart, rangeEnd, limit);
 
 	return dates.map((date) => {
 		const startDate = DateTime.fromJSDate(date);
 		const calendarLink = ics({
 			title: faker.lorem.sentence(7),
-			start: startDate.toString(),
-			end: startDate.plus({ hours: 1 }),
+			start: startDate.toUTC().toString(),
+			end: startDate.toUTC().plus({ hours: 1 }).toString(),
 			description: faker.lorem.paragraph(),
 		});
 
 		return {
 			id: faker.datatype.uuid(),
 			title: faker.lorem.sentence(7),
-			startDateLocalized: startDate.toString(),
-			endDateLocalized: startDate.plus({ hours: 1 }),
+			startDateLocalized: startDate.toUTC().toString(),
+			endDateLocalized: startDate.toUTC().plus({ hours: 1 }).toString(),
 			eventCalendarDescription: `<p>${faker.lorem.paragraph()}</p>`,
 			eventCalendarLink: calendarLink,
 		};
--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,20 +1,31 @@
 import VirtualCoffeeFullBanner from '~/svg/VirtualCoffeeFullBanner';
-import type { LoaderArgs } from '@remix-run/node';
+import type { LoaderFunction } from '@remix-run/node';
 import getSponsors from '~/data/sponsors';
+import type { SponsorsResponse } from '~/data/sponsors';
 import { json } from '@remix-run/node';
 import { useLoaderData } from '@remix-run/react';
 import { getEvents } from '~/data/events';
-import { dateForDisplay } from '~/util/date';
+import type { EventsResponse } from '~/data/events';
 import { getEpisodes } from '~/data/podcast';
 import HomePageBlock from '~/components/HomePageBlock';
 import PostList, {
 	formatFileListItemsForPostList,
 } from '~/components/PostList';
+import { dateForDisplay } from '~/util/date';
 import { loadMdxDirectory } from '~/util/loadMdx.server';
 import getNewsletters from '~/data/newsletters';
 import getChallenges from '~/data/monthlyChallenges/getChallenges';
 
-export async function loader(args: LoaderArgs) {
+interface LoaderData {
+	sponsors: SponsorsResponse;
+	events: EventsResponse;
+	podcastEpisodes: any;
+	resources: any;
+	newsletters: any;
+	challenges: any;
+}
+
+export const loader: LoaderFunction = async () => {
 	const [sponsors, events, podcastEpisodes, newsletters, challenges] =
 		await Promise.all([
 			getSponsors(),
@@ -39,7 +50,7 @@ export async function loader(args: LoaderArgs) {
 		newsletters,
 		challenges,
 	});
-}
+};
 
 export const homePageLinks = [
 	{
@@ -147,12 +158,19 @@ export default function Index() {
 							footer="See more Community Events"
 						>
 							<PostList
-								items={events.map((event: any) => ({
-									title: event.title,
-									description: (
-										<strong>{dateForDisplay(event.startDateLocalized)}</strong>
-									),
-								}))}
+								items={events.map((event) => {
+									const eventTime = event.startDateLocalized;
+									return {
+										title: event.title,
+										description: (
+											<strong>
+												<time suppressHydrationWarning dateTime={eventTime}>
+													{dateForDisplay(eventTime)}
+												</time>
+											</strong>
+										),
+									};
+								})}
 							/>
 						</HomePageBlock>
 					</div>
