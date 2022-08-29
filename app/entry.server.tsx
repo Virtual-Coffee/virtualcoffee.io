ebf18a30b32013b5ab6ad0710b1c8841595bfd1e
Dan Ott <dan@dtott.com>
2022-08-29 11:05:35 -0400

    upgrade remix dependencies and scripts

--- app/entry.server.tsx
+++ app/entry.server.tsx
@@ -1,7 +1,6 @@
 import type { EntryContext } from '@remix-run/node';
-import { renderToString } from 'react-dom/server';
 import { RemixServer } from '@remix-run/react';
-import { cacheControlValues } from './util/http';
+import { renderToString } from 'react-dom/server';
 
 if (process.env.NETLIFY_DEV) {
 	process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
@@ -19,28 +18,8 @@ export default function handleRequest(
 
 	responseHeaders.set('Content-Type', 'text/html');
 
-	// this may change in the future, but for now, default to caching all pages.
-	// this can be overwritten in any page by setting a Cache-Control header
-	if (!responseHeaders.get('Cache-Control')) {
-		responseHeaders.set('Cache-Control', cacheControlValues.standard);
-	}
-
 	return new Response('<!DOCTYPE html>' + markup, {
 		status: responseStatusCode,
 		headers: responseHeaders,
 	});
-}
-
-export function handleDataRequest(
-	response: Response,
-	// same args that get passed to the action or loader that was called
-	// { request, params, context },
-) {
-	// this may change in the future, but for now, default to caching all data requests.
-	// this can be overwritten in any loader by setting a Cache-Control header
-	if (!response.headers.get('Cache-Control')) {
-		response.headers.set('Cache-Control', cacheControlValues.standard);
-	}
-
-	return response;
 }
