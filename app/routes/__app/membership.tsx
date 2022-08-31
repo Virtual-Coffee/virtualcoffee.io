999cda1a072ce59e08b9ab0114f66c443d805984
Dan Ott <dan@dtott.com>
2022-08-31 15:27:26 -0400

    removing

--- app/routes/__app/membership.tsx
+++ app/routes/__app/membership.tsx
@@ -1,21 +1,4 @@
-import { json } from '@remix-run/node';
-import type { LoaderFunction } from '@remix-run/node';
-import { Link } from '@remix-run/react';
-import { authenticate } from '~/auth/auth.server';
 import { Outlet } from '@remix-run/react';
-import DefaultLayout from '~/components/layouts/DefaultLayout';
-
-export const loader: LoaderFunction = async ({ request }) => {
-	let user = await authenticate(request);
-	if (user) {
-		// console.log({ user: user?.authenticate?.user });
-		// console.log({ user });
-	} else {
-		console.log('no user');
-	}
-
-	return json({ x: 1 });
-};
 
 export default function Page() {
 	return <Outlet />;
