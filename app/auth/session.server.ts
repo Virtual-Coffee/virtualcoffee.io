0bb33769e8e6605cba6f7feaca7f5e1eec2cc501
Dan Ott <dan@dtott.com>
2022-08-31 15:35:16 -0400

    add note

--- app/auth/session.server.ts
+++ app/auth/session.server.ts
@@ -14,6 +14,9 @@ function configSessionStorage() {
 	});
 }
 
+// this is needed because in development we don't want to restart
+// the server with every change, but we want to make sure we don't
+// create a new connection to the DB with every change either.
 let sessionStorage: ReturnType<typeof createCookieSessionStorage>;
 
 declare global {
