176d1cdc3b7a5acbc1f43f9f819f0648bbd0cf98
Dan Ott <dan@dtott.com>
2022-08-31 16:19:15 -0400

    use custom livereload from 1.6.4 of remix
    see https://github.com/remix-run/remix/issues/2997#issuecomment-1233379949

--- app/root.jsx
+++ app/root.jsx
@@ -2,7 +2,6 @@ import { json } from '@remix-run/node';
 import {
 	Outlet,
 	Links,
-	LiveReload,
 	Meta,
 	Scripts,
 	ScrollRestoration,
@@ -75,6 +74,49 @@ export function meta({ data: { meta } = {} } = {}) {
 		'theme-color': '#ffffff',
 	};
 }
+
+export const LiveReload =
+	process.env.NODE_ENV !== 'development'
+		? () => null
+		: function LiveReload({
+				port = Number(process.env.REMIX_DEV_SERVER_WS_PORT || 8002),
+				nonce = undefined,
+		  }) {
+				let js = String.raw;
+				return (
+					<script
+						nonce={nonce}
+						suppressHydrationWarning
+						dangerouslySetInnerHTML={{
+							__html: js`
+							(() => {
+                  let protocol = location.protocol === "https:" ? "wss:" : "ws:";
+                  let host = location.hostname;
+                  let socketPath = protocol + "//" + host + ":" + ${String(
+										port,
+									)} + "/socket";
+                  let ws = new WebSocket(socketPath);
+                  ws.onmessage = (message) => {
+                    let event = JSON.parse(message.data);
+                    if (event.type === "LOG") {
+                      console.log(event.message);
+                    }
+                    if (event.type === "RELOAD") {
+                      console.log("💿 Reloading window ...");
+                      window.location.reload();
+                    }
+                  };
+
+                  ws.onerror = (error) => {
+                    console.log("Remix dev asset server web socket error:");
+                    console.error(error);
+                  };
+                })();
+              `,
+						}}
+					/>
+				);
+		  };
 
 export default function App() {
 	const location = useLocation();
