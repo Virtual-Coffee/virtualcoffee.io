ee172cafd953b092f991aa5a262dd912c00fa23a
Dan Ott <dan@dtott.com>
2022-08-31 15:26:21 -0400

    move tls reject to env variable

--- app/entry.server.tsx
+++ app/entry.server.tsx
@@ -2,10 +2,6 @@ import type { EntryContext } from '@remix-run/node';
 import { RemixServer } from '@remix-run/react';
 import { renderToString } from 'react-dom/server';
 
-if (process.env.NETLIFY_DEV) {
-	process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
-}
-
 export default function handleRequest(
 	request: Request,
 	responseStatusCode: number,
