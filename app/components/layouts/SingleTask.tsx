2de5fde85d6db62c62f0c1d37de01caee9e45a7e
Dan Ott <dan@dtott.com>
2022-09-07 16:03:47 -0400

    add wide option

--- app/components/layouts/SingleTask.tsx
+++ app/components/layouts/SingleTask.tsx
@@ -1,15 +1,23 @@
+import classNames from 'classnames';
 import VirtualCoffeeFullBanner from '~/svg/VirtualCoffeeFullBanner';
 
 export default function SingleTask({
 	children,
 	title,
+	wide,
 }: {
 	title?: React.ReactNode;
 	children?: React.ReactNode;
+	wide?: boolean;
 }) {
 	return (
 		<div className="min-h-full flex flex-col justify-center py-12 sm:px-6 lg:px-8">
-			<div className="sm:mx-auto sm:w-full sm:max-w-md">
+			<div
+				className={classNames(
+					'sm:mx-auto sm:w-full',
+					wide ? 'sm:max-w-2xl' : 'sm:max-w-md',
+				)}
+			>
 				<VirtualCoffeeFullBanner
 					className="mx-auto h-12 w-auto"
 					title="Virtual Coffee"
@@ -20,7 +28,12 @@ export default function SingleTask({
 					</h2>
 				)}
 			</div>
-			<div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
+			<div
+				className={classNames(
+					'mt-8 sm:mx-auto sm:w-full',
+					wide ? 'sm:max-w-2xl' : 'sm:max-w-md',
+				)}
+			>
 				<div className="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
 					{children}
 				</div>
