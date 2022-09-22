22c35cf617fa53c902dc9a5b5c9ea3238f71f6ab
Dan Ott <dan@dtott.com>
2022-09-22 11:06:10 -0400

    update copy to use button instead of embed

--- app/routes/join/index.jsx
+++ app/routes/join/index.jsx
@@ -16,7 +16,7 @@ export function meta({ data: { meta } }) {
 	return meta;
 }
 
-export default function CocForm() {
+export default function Join() {
 	return (
 		<DefaultLayout
 			simple
@@ -24,33 +24,33 @@ export default function CocForm() {
 			heroHeader="Join Virtual Coffee"
 			heroSubheader="Learn how to join our community"
 		>
-			<div className="lead mb-5">
-				<p>
-					Virtual Coffee is a community that welcomes people at all stages of
-					their tech journey. Our mission is to be a welcoming tech community
-					that allows room for growth and mentorship at all levels, and to
-					create meaningful opportunities for learning, leadership, and
-					contribution for everyone.
-				</p>
-				<p>
-					To preserve what makes Virtual Coffee special, we intentionally keep
-					our group small. We'd love to have everyone as a part of Virtual
-					Coffee, but we prioritize the intimacy and closeness of the group. Our
-					community is currently accepting members on a limited basis. As new
-					membership becomes available, we'll reach out to those on the waitlist
-					to join. To join the waitlist, please fill out the form below.
-				</p>
+			<div className="prose">
+				<div className="lead mb-5">
+					<p>
+						Virtual Coffee is a community that welcomes people at all stages of
+						their tech journey. Our mission is to be a welcoming tech community
+						that allows room for growth and mentorship at all levels, and to
+						create meaningful opportunities for learning, leadership, and
+						contribution for everyone.
+					</p>
+					<p>
+						To preserve what makes Virtual Coffee special and support our
+						existing members, we intentionally keep our group small. We'd love
+						to have everyone as a part of Virtual Coffee, but we prioritize the
+						intimacy and closeness of the group. Our community is currently
+						accepting members on a limited basis. As new membership becomes
+						available, we'll reach out to those on the wait list to join.
+					</p>
+					<div className="mt-5 text-center">
+						<a
+							href="https://airtable.com/shrWOl22B5iKYADub"
+							className="btn btn-primary btn-lg"
+						>
+							Join the Wait List
+						</a>
+					</div>
+				</div>
 			</div>
-			<iframe
-				title="new membership form"
-				class="airtable-embed"
-				src="https://airtable.com/embed/shrWOl22B5iKYADub?backgroundColor=gray"
-				frameborder="0"
-				onmousewheel=""
-				width="100%"
-				height="533"
-				style={{ background: 'transparent', border: '1px solid #ccc' }}
-			></iframe>
 		</DefaultLayout>
 	);
 }
