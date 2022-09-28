76d15494a65ba9d6267fee503d475a3bd5fd5c1a
Dan Ott <dan@dtott.com>
2022-09-28 12:16:54 -0400

    do not default to empty string

--- app/components/layouts/DefaultLayout.jsx
+++ app/components/layouts/DefaultLayout.jsx
@@ -86,9 +86,9 @@ export function HeroHead({ Hero, heroHeader, heroSubheader, simple }) {
 }
 
 export default function DefaultLayout({
-	Hero = '',
-	heroHeader = '',
-	heroSubheader = '',
+	Hero,
+	heroHeader,
+	heroSubheader,
 	simple = false,
 	showHero = true,
 	children,
