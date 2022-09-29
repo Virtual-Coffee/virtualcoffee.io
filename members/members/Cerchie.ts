319081691216045a949891353efb463be96dc6c6
Dan Ott <dan@dtott.com>
2022-09-29 16:20:32 -0400

    convert to ts

---
+++ members/members/Cerchie.ts
@@ -0,0 +1,31 @@
+import type { MemberObject } from '../types';
+
+export const Cerchie: MemberObject = {
+	// GitHub username (required)
+	github: 'Cerchie',
+	//
+	// Everything below here is optional. By default, we pull most profile data from your GitHub profile. You can override that data here, as well as provide some additional account links below.
+	//
+	// Name - If not defined here, it will default to your display name on GitHub. If that's not defined, then your GitHub username.
+	// name: 'Your Name',
+	//
+	// Main URL - If not defined here, it will default to the website displayed on your GitHub profile. If that's not defined, then a link to your GitHub profile will be displayed.
+	// mainUrl: 'https://virtualcoffee.io',
+	//
+	// Bio - Accepts [markdown](https://spec.commonmark.org/0.30/). Please keep your bio to a reasonable length. Refer to our [members page](https://virtualcoffee.io/members/) for examples.
+	// bio: `This is _my_ **bio** and [here is a link](https://virtualcoffee.io)`,
+	//
+	// Links - You can add one of each type, except website - you can add as many `website` accounts as you wish.
+	accounts: [
+		// { type: 'linkedin', username: 'yourlinkedinUserName' },
+		// { type: 'dev', username: 'yourUserName' },
+		// { type: 'codenewbie', username: 'yourUserName' },
+		// { type: 'twitter', username: 'yourUserName' },
+		// { type: 'twitch', username: 'yourUserName' },
+		// { type: 'youtube', channelId: 'yourChannelId' }, OR { type: 'youtube', customUrl: 'https://www.youtube.com/c/yourCustomUrl' },
+		// { type: 'polywork', username: 'yourUserName' },
+		// { type: 'medium', username: 'yourUserName' },
+		// { type: 'hashnode', username: 'yourUserName' },
+		// { type: 'website', url: 'https://virtualcoffee.io', title: 'Title of link' },
+	],
+};
---
+++ members/members/magsol.ts
@@ -0,0 +1,32 @@
+import type { MemberObject } from '../types';
+
+export const magsol: MemberObject = {
+	// GitHub username (required)
+	github: 'magsol',
+	//
+	// Everything below here is optional. By default, we pull most profile data from your GitHub profile. You can override that data here, as well as provide some additional account links below.
+	//
+	// Name - If not defined here, it will default to your display name on GitHub. If that's not defined, then your GitHub username.
+	name: 'Shannon Quinn',
+	//
+	// Main URL - If not defined here, it will default to the website displayed on your GitHub profile. If that's not defined, then a link to your GitHub profile will be displayed.
+	mainUrl: 'https://magsol.github.io',
+	//
+	// Bio - Accepts [markdown](https://spec.commonmark.org/0.30/). Please keep your bio to a reasonable length. Refer to our [members page](https://virtualcoffee.io/members/) for examples. Please keep your bio to a reasonable length, refer to
+	// [members page](https://virtualcoffee.io/members/) for examples
+	bio: 'Academic, Data Scientist, Runner, Donut-eater',
+	//
+	// Links - You can add one of each type, except website - you can add as many `website` accounts as you wish.
+	accounts: [
+		{ type: 'linkedin', username: 'shannonpquinn' },
+		// { type: 'dev', username: 'yourUserName' },
+		// { type: 'codenewbie', username: 'yourUserName' },
+		{ type: 'twitter', username: 'SpectralFilter' },
+		// { type: 'twitch', username: 'yourUserName' },
+		// { type: 'youtube', channelId: 'yourChannelId' }, OR { type: 'youtube', customUrl: 'https://www.youtube.com/c/yourCustomUrl' },
+		// { type: 'polywork', username: 'yourUserName' },
+		// { type: 'medium', username: 'yourUserName' },
+		// { type: 'hashnode', username: 'yourUserName' },
+		// { type: 'website', url: 'https://virtualcoffee.io', title: 'Title of link' },
+	],
+};
