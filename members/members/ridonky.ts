17f7e22b0c1034a55db64b9d163acfa9a5ca7ce7
Lauren Perini <75059734+ridonky@users.noreply.github.com>
2022-10-03 14:53:17 -0400

    fixed const variable name to match username
    changed default "_EXAMPLE" to "ridonky"

--- members/members/ridonky.ts
+++ members/members/ridonky.ts
@@ -1,7 +1,7 @@
 import type { MemberObject } from '../types';
 import { profileMasks } from '../flare';
 
-export const _EXAMPLE: MemberObject = {
+export const ridonky: MemberObject = {
 	// GitHub username (required)
 	github: 'ridonky',
 	//
