0ac69d923d5973782c6e24334a4981a5613aeabc
alexvcs <alexcurtisslep@gmail.com>
2022-10-04 19:05:43 -0400

    added the Hacktoberfest2022 badge :)

--- members/members/AlexVCS.ts
+++ members/members/AlexVCS.ts
@@ -42,5 +42,5 @@ export const AlexVCS: MemberObject = {
 		// { type: 'hashnode', username: 'yourUserName' },
 		// { type: 'website', url: 'https://virtualcoffee.io', title: 'Title of link' },
 	],
-	badges: [],
+	badges: ['Hacktoberfest2022'],
 };
