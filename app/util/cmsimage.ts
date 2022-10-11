43716ae91be194c4130048a6dc1ed595290536d0
Joe Karow <58997957+JoeKarow@users.noreply.github.com>
2022-10-11 11:13:05 -0400

    add JSDoc documentation

--- app/util/cmsimage.ts
+++ app/util/cmsimage.ts
@@ -1,5 +1,13 @@
 import ImgixClient from '@imgix/js-core';
 
+/**
+ * It takes a path and folder and returns a URL to an image on Imgix
+ * @arg {CreateCmsImageProps} - path, folder, and (optional) settings
+ * @prop {string} `path` - the image file name
+ * @prop {string} `folder` - the preceding directory/directories before the file name
+ * @prop {object} [`settings`={}] - any other {@link https://docs.imgix.com/apis/rendering rendering options} for the Imgix library
+ * @returns A URL to the image
+ */
 export default function createCmsImage({
 	path,
 	folder,
@@ -15,8 +23,14 @@ export default function createCmsImage({
 	});
 }
 
+/**
+ * Properties for the `createCmsImage` function
+ * @property {string} `path` - the image file name
+ * @property {string} `folder` - the preceding directory/directories before the file name
+ * @property {object} [`settings`={}] - any other {@link https://docs.imgix.com/apis/rendering rendering options} for the Imgix library
+ */
 type CreateCmsImageProps = {
 	path: string;
 	folder: string;
-	settings: Record<string, any>;
+	settings?: Record<string, any>;
 };
