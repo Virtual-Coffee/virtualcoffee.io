803da2ea53cfe81768144c7b178da6acb05f7dfc
imani.devs <imaniroberts.dev@gmail.com>
2022-10-14 11:15:49 -0400

    add hacktoberfest2022 badge

--- members/members/imanidev.ts
+++ members/members/imanidev.ts
@@ -42,5 +42,5 @@ export const imanidev: MemberObject = {
 		// { type: 'hashnode', username: 'yourUserName' },
 		// { type: 'website', url: 'https://virtualcoffee.io', title: 'Title of link' },
 	],
-	badges: ['Hacktoberfest 2022'],
+	badges: ['Hacktoberfest2022'],
 };
