5dc6ab305933a6423000a1678a8b637cefc2c6e2
Dominic Duffin <dominicduffin@outlook.com>
2022-10-14 16:47:01 +0100

    Add Hacktoberfest 2022 badge to my profile

--- members/members/dominicduffin1.ts
+++ members/members/dominicduffin1.ts
@@ -42,5 +42,5 @@ export const dominicduffin1: MemberObject = {
 		// { type: 'hashnode', username: 'yourUserName' },
 		// { type: 'website', url: 'https://virtualcoffee.io', title: 'Title of link' },
 	],
-	badges: [],
+	badges: ['Hacktoberfest2022'],
 };
