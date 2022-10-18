ab5ab43234b2810499c7bc589e7b5b27b9f43bbe
Sadie <19538219+sadiejay@users.noreply.github.com>
2022-10-18 12:26:07 -0500

    Convert ts undrawillustration (#721)
    * Renamed file
    
    * Added type to UndrawIllustration
    
    * Add ReactElement return type
    
    Co-authored-by: Dan Ott <dan@dtott.com>

--- app/components/UndrawIllustration.tsx
+++ app/components/UndrawIllustration.tsx
@@ -66,7 +66,17 @@ const svgAspectRatios = {
 	UndrawConversation: '842 / 842.79999',
 };
 
-export default function UndrawIllustration({ filename, style = {}, ...props }) {
+type UndrawIllustrationProps = {
+	/** Filename of svg file found in [`public/assets/svg`](https://github.com/Virtual-Coffee/virtualcoffee.io/tree/main/public/assets/svg) */
+	filename: keyof typeof svgAspectRatios;
+	style?: React.CSSProperties;
+} & React.ImgHTMLAttributes<HTMLImageElement>;
+
+export default function UndrawIllustration({
+	filename,
+	style = {},
+	...props
+}: UndrawIllustrationProps): React.ReactElement<'img'> {
 	const aspectRatio = svgAspectRatios[filename];
 
 	return (
