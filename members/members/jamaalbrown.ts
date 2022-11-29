6f55b8d65f26446d25fb68676b5111621d24ba54
Jamaal Brown <59902352+Jamaalwbrown@users.noreply.github.com>
2022-11-29 15:47:17 -0600

    Replace jamaalbrown.js with jamaalbrown.ts (#748)

--- members/members/jamaalbrown.ts
+++ members/members/jamaalbrown.ts
@@ -1,20 +1,33 @@
-module.exports = {
+import type { MemberObject } from '../types';
+// import { profileMasks } from '../flare';
+
+export const Jamaalwbrown: MemberObject = {
 	// GitHub username (required)
 	github: 'Jamaalwbrown',
 	//
 	// Everything below here is optional. By default, we pull most profile data from your GitHub profile. You can override that data here, as well as provide some additional account links below.
 	//
 	// Name - If not defined here, it will default to your display name on GitHub. If that's not defined, then your GitHub username.
-	// name: 'Your Name',
-	name: 'Jamaal Brown',
+	 name: 'Jamaal Brown',
 	//
 	// Main URL - If not defined here, it will default to the website displayed on your GitHub profile. If that's not defined, then a link to your GitHub profile will be displayed.
-	// mainUrl: 'https://virtualcoffee.io',
-	//
-	mainUrl: 'https://jamaalbrown.me',
+	 mainUrl: 'https://jamaalbrown.me',
 	//
 	// Bio - Accepts [markdown](https://spec.commonmark.org/0.30/). Please keep your bio to a reasonable length. Refer to our [members page](https://virtualcoffee.io/members/) for examples.
-	bio: `Full-stack software engineer with a passion for learning. Coffee and tea enthusiast as well!`,
+	 bio: `Full-stack software engineer with a passion for learning. Coffee and tea enthusiast as well!`,
+	//
+	// flare - If you want to add a flare to your profile, you can do so here.
+	// Uncomment the `import { profileMasks } from '../flare';` line at the top of this file to use the profileMasks.
+	// So far  all we have is a profile mask, which makes your profile picture a different shape from the default square.
+	// Leave this out if you prefer the default square.
+	// You can choose from the following profile masks:
+	// profileMasks.octogon, profileMasks.hexagon, profileMasks.triangle,
+	// profileMasks.circle, profileMasks.rabbet, profileMasks.star
+	// you can also use a custom string. the profileMask values are [css clipPath values](https://developer.mozilla.org/en-US/docs/Web/CSS/clip-path)
+	flare: {
+		// example:
+		// profileMask: profileMasks.triangle,
+	},
 	//
 	// Links - You can add one of each type, except website - you can add as many `website` accounts as you wish.
 	accounts: [
@@ -29,4 +42,5 @@ module.exports = {
 		// { type: 'hashnode', username: 'yourUserName' },
 		// { type: 'website', url: 'https://virtualcoffee.io', title: 'Title of link' },
 	],
+	badges: [],
 };
