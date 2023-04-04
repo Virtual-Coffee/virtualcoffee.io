56e2e6e864f67934466533ac9655a4a2b4e2301b
Dan Ott <dan@dtott.com>
2023-04-03 21:03:23 -0400

    update styles

--- app/routes/__frontend/monthlychallenges/index.jsx
+++ app/routes/__frontend/monthlychallenges/index.jsx
@@ -29,11 +29,9 @@ export function meta({ data: { meta } = {} } = {}) {
 export default function Index() {
 	return (
 		<div>
-			<div className="bg-light py-3">
-				<div className="container-xl">
-					<h2 className="display-5">
-						Why should members participate in the monthly challenge?
-					</h2>
+			<div className="bg-white py-3">
+				<div className="container">
+					<h2>What are monthly challenges?</h2>
 
 					<p>
 						These monthly challenges provide members the opportunity to learn,
@@ -44,12 +42,8 @@ export default function Index() {
 						Maintainers, our Challenge Team Leads plan, organize, and facilitate
 						these challenges.
 					</p>
-				</div>
-			</div>
 
-			<div className="bg-light py-3">
-				<div className="container-xl">
-					<h2 className="display-5">Who can participate?</h2>
+					<h2>Who can participate?</h2>
 
 					<p>
 						These challenges are available to all Virtual Coffee members and the
@@ -64,177 +58,191 @@ export default function Index() {
 			</div>
 
 			<div className="bg-light py-5">
-				<div className="container-xl">
+				<div className="container">
 					<h2 className="display-5">Our challenges:</h2>
-					<h3>Month of Learning</h3>
+					<dl className="gridlist mt-4">
+						<dt>Month of Learning</dt>
+						<dd>
+							<p className="gridlist-subtitle">Time to learn new things!</p>
+							<p>
+								The goal this challenge is to learn something new, share what we
+								have learned, and gather recommendations and resources to share
+								with the community. During this challenge, we work on learning
+								new dev-related things. You might deep-dive into one topic,
+								start a small-group that focuses on community learning, focus on
+								a new topic every week, or do a little bit of everything.
+							</p>
+						</dd>
 
-					<dl>
-						<dt>Time to learn new things!</dt>
+						<dt>Get job ready!</dt>
 						<dd>
-							The goal this challenge is to learn something new, share what we
-							have learned, and gather recommendations and resources to share
-							with the community. During this challenge, we work on learning new
-							dev-related things. You might deep-dive into one topic, start a
-							small-group that focuses on community learning, focus on a new
-							topic every week, or do a little bit of everything.
+							<p className="gridlist-subtitle">
+								Resumes, Cover Letters, and Elevator Pitch. There's never a bad
+								time to update your job application materials.
+							</p>
+							<p>
+								The goal of this challenge is to work on creating, revising, or
+								updating your job packet materials and that elevator pitch that
+								might get you in the door. Your resume, cover letter, and
+								elevator pitch should work together to tell your story and
+								represent where you are on your career journey; each piece
+								should complement the others. This challenge emphasizes taking
+								time to make sure they work together or to get some extra
+								feedback on what you've worked on.{' '}
+							</p>
 						</dd>
-					</dl>
-					<h3>Get job ready! Resumes, Cover Letters, and Elevator Pitch</h3>
 
-					<dl>
-						<dt>
-							There's never a bad time to update your job application materials.
-						</dt>
+						<dt>Creative Community Challenge</dt>
 						<dd>
-							The goal of this challenge is to work on creating, revising, or
-							updating your job packet materials and that elevator pitch that
-							might get you in the door. Your resume, cover letter, and elevator
-							pitch should work together to tell your story and represent where
-							you are on your career journey; each piece should complement the
-							others. This challenge emphasizes taking time to make sure they
-							work together or to get some extra feedback on what you've worked
-							on.{' '}
+							<p className="gridlist-subtitle">
+								Let's make some space for the other parts of ourselves.
+							</p>
+							<p>
+								Devs are more than just the code we write. This challenge is all
+								about embracing self-expression. Give back to yourself by
+								indulging in something just for fun. Share the art, music,
+								poetry, sports, games, or any other hobbies that spark joy for
+								you. We spend so much time grinding away on understanding things
+								in the tech space. Let's make some space for the other parts of
+								ourselves. In this challenge, we encourage folks to spend time
+								working on things that aren't necessarily code specific, or
+								using code to improve other hobbies and outlets.
+							</p>
 						</dd>
-					</dl>
-					<h3>Creative Community Challenge</h3>
-
-					<dl>
-						<dt>Let's make some space for the other parts of ourselves.</dt>
+						<dt>Blogging Challenge</dt>
 						<dd>
-							Devs are more than just the code we write. This challenge is all
-							about embracing self-expression. Give back to yourself by
-							indulging in something just for fun. Share the art, music, poetry,
-							sports, games, or any other hobbies that spark joy for you. We
-							spend so much time grinding away on understanding things in the
-							tech space. Let's make some space for the other parts of
-							ourselves. In this challenge, we encourage folks to spend time
-							working on things that aren't necessarily code specific, or using
-							code to improve other hobbies and outlets.
+							<p className="gridlist-subtitle">
+								A Community Challenge to hit a word count goal for all our tech
+								blogs.
+							</p>
+							<p>
+								Based off the NaNoWriMo (National Novel Writing Month)
+								Challenge, this challenge is the tech take on writing and
+								working together towards the goal while posting on our own
+								blogs.
+							</p>
 						</dd>
-					</dl>
-					<h3>Blogging Challenge</h3>
-					<dl>
-						<dt>
-							A Community Challenge to hit a word count goal for all our tech
-							blogs.
-						</dt>
+						<dt>Hacktoberfest</dt>
 						<dd>
-							Based off the NaNoWriMo (National Novel Writing Month) Challenge,
-							this challenge is the tech take on writing and working together
-							towards the goal while posting on our own blogs.
+							<p className="gridlist-subtitle">
+								Participate in open source, learn, and have fun!
+							</p>
+							<p>
+								This challenge is always run during October and was our first
+								ever monthly challenge. We have three tracks: maintainers will
+								provide issues labeled for Hacktoberfest, contributors will
+								solve issues, and mentors will help contributors and maintainers
+								be successful.
+							</p>
 						</dd>
-					</dl>
-					<h3>Hacktoberfest</h3>
-					<dl>
-						<dt>Participate in open source, learn, and have fun!</dt>
+						<dt>Preptember</dt>
 						<dd>
-							This challenge is always run during October and was our first ever
-							monthly challenge. We have three tracks: maintainers will provide
-							issues labeled for Hacktoberfest, contributors will solve issues,
-							and mentors will help contributors and maintainers be successful.
+							<p className="gridlist-subtitle">
+								Get your open source projects ready!
+							</p>
+							<p>
+								Maintainers will be reviewing their open source repos with our
+								checklist to make sure their projects are ready for
+								Hacktoberfest contributions, and our contributors will be
+								looking at their favorite repos, evaluating them based on the
+								guide, and writing good issues as needed to fulfill the
+								criteria.
+							</p>
 						</dd>
-					</dl>
-					<h3>Preptember</h3>
-					<dl>
-						<dt>Get your open source projects ready!</dt>
+						<dt>Healthy Habits for Happy Devs</dt>
 						<dd>
-							Maintainers will be reviewing their open source repos with our
-							checklist to make sure their projects are ready for Hacktoberfest
-							contributions, and our contributors will be looking at their
-							favorite repos, evaluating them based on the guide, and writing
-							good issues as needed to fulfill the criteria.
+							<p className="gridlist-subtitle">
+								This month's challenge is all about nourishing our bodies,
+								minds, and spirits so that we can become healthier developers.
+							</p>
+							<p>
+								The goal of this challenge is to build a new habit that will
+								make you a healthier dev; this can be mind and body centered
+								(drink, move, read, meditate, rearrange your work station) or
+								code centered (review your ReadMe, clean your code, refresh your
+								GitHub repo) or both. Set the goal for yourself this month and
+								define what successfully completing the challenge looks like.
+								For example, could be something like: review the README in 5 of
+								your projects (one every week) or run 2k twice a week.
+							</p>
 						</dd>
-					</dl>
-					<h3>Healthy Habits for Happy Devs</h3>
-					<dl>
-						<dt>
-							This month's challenge is all about nourishing our bodies, minds,
-							and spirits so that we can become healthier developers.
-						</dt>
+						<dt>Build in Public</dt>
 						<dd>
-							The goal of this challenge is to build a new habit that will make
-							you a healthier dev; this can be mind and body centered (drink,
-							move, read, meditate, rearrange your work station) or code
-							centered (review your ReadMe, clean your code, refresh your GitHub
-							repo) or both. Set the goal for yourself this month and define
-							what successfully completing the challenge looks like. For
-							example, could be something like: review the README in 5 of your
-							projects (one every week) or run 2k twice a week.
+							<p className="gridlist-subtitle">
+								Communicate what you're working on, show your development, and
+								be confident and proud of any progress made.
+							</p>
+							<p>
+								In this challenge, we're working on creating a habit of talking
+								about the things we're working on, a plan for continuing
+								progress, and creating a demo for the Virtal Coffee community.
+							</p>
 						</dd>
-					</dl>
-					<h3>Build in Public</h3>
-					<dl>
-						<dt>
-							Communicate what you're working on, show your development, and be
-							confident and proud of any progress made.
-						</dt>
+						<dt>Community Kindness</dt>
 						<dd>
-							In this challenge, we're working on creating a habit of talking
-							about the things we're working on, a plan for continuing progress,
-							and creating a demo for the Virtal Coffee community.
+							<p className="gridlist-subtitle">
+								Celebrating our Community as we move into a new year of Virtual
+								Coffee!
+							</p>
+							<p>
+								As we work hard to make sure this community continues to be the
+								special and close-knit group, this challenge encourages our
+								members to celebrate one of the things that continually makes
+								this community so special: Kindness. Some of the ways we see
+								this include: practicing gratitude, reaching out to support
+								other members, mentoring, helping, giving honest and
+								constructive feedback, and continuing to make Virtual Coffee a
+								safe and supportive space.
+							</p>
 						</dd>
-					</dl>
-					<h3>Community Kindness</h3>
-					<dl>
-						<dt>
-							Celebrating our Community as we move into a new year of Virtual
-							Coffee!
-						</dt>
+						<dt>Creating Audio/Visual content</dt>
 						<dd>
-							As we work hard to make sure this community continues to be the
-							special and close-knit group, this challenge encourages our
-							members to celebrate one of the things that continually makes this
-							community so special: Kindness. Some of the ways we see this
-							include: practicing gratitude, reaching out to support other
-							members, mentoring, helping, giving honest and constructive
-							feedback, and continuing to make Virtual Coffee a safe and
-							supportive space.
+							<p className="gridlist-subtitle">
+								Create a community of knowledge sharing and access to learning
+								with AV content.
+							</p>
+							<p>
+								For this challenge, members present their knowledge and showcase
+								your understanding by exploring video and audio mediums for
+								sharing knowledge, highlighting their achievements. They might
+								do that by giving lunch-and-learns, youtube videos, podcasts, or
+								some other form of audio or video content they created that
+								explores a coding-related concept--there's no length
+								requirement. We believe this provides value by solidifying
+								ideas, creating a resource for others, inviting personal growth
+								through conversations sparked by sharing, and demonstrating your
+								ability to talk through a concept.
+							</p>
 						</dd>
-					</dl>
-					<h3>Creating Audio/Visual content</h3>
-					<dl>
-						<dt>
-							Create a community of knowledge sharing and access to learning
-							with AV content.
-						</dt>
+						<dt>Pairing</dt>
 						<dd>
-							For this challenge, members present their knowledge and showcase
-							your understanding by exploring video and audio mediums for
-							sharing knowledge, highlighting their achievements. They might do
-							that by giving lunch-and-learns, youtube videos, podcasts, or some
-							other form of audio or video content they created that explores a
-							coding-related concept--there's no length requirement. We believe
-							this provides value by solidifying ideas, creating a resource for
-							others, inviting personal growth through conversations sparked by
-							sharing, and demonstrating your ability to talk through a concept.
+							<p className="gridlist-subtitle">
+								Pairing is more than just coding with someone else. Pairing is
+								about communication, teaching, learning, positive
+								reinforcements, and growing.
+							</p>
+							<p>
+								For this challenge, members are challenged to hit 5 pairing
+								sessions per person, limiting your pairing sessions to one
+								Pomodoro session--twenty-five minute. Some ways to get started
+								pairing are on an open-source issue, a LeetCode problem, or a
+								project they need help on.
+							</p>
 						</dd>
-					</dl>
-					<h3>Pairing</h3>
-					<dl>
-						<dt>
-							Pairing is more than just coding with someone else. Pairing is
-							about communication, teaching, learning, positive reinforcements,
-							and growing.
-						</dt>
+						<dt>Month of Feedback</dt>
 						<dd>
-							{' '}
-							For this challenge, members are challenged to hit 5 pairing
-							sessions per person, limiting your pairing sessions to one
-							Pomodoro session--twenty-five minute. Some ways to get started
-							pairing are on an open-source issue, a LeetCode problem, or a
-							project they need help on.
-						</dd>
-					</dl>
-					<h3>Giving Meaningful and Empathetic Feedback</h3>
-					<dl>
-						<dt>Give and accept feedback from others.</dt>
-						<dd>
-							When we care about our community members, we can offer empathetic
-							responses that are honest and allow them to grow and to fix a
-							problem. Sometimes these conversations are hard. But sometimes we
-							need hard conversations to help us grow. This challenge calls
-							members to approach each other with kindness and honesty, and
-							allow this kind of feedback to be a regular part of our process.
+							<p className="gridlist-subtitle">
+								Giving and accepting meaningful and empathetic feedback.
+							</p>
+							<p>
+								When we care about our community members, we can offer
+								empathetic responses that are honest and allow them to grow and
+								to fix a problem. Sometimes these conversations are hard. But
+								sometimes we need hard conversations to help us grow. This
+								challenge calls members to approach each other with kindness and
+								honesty, and allow this kind of feedback to be a regular part of
+								our process.
+							</p>
 						</dd>
 					</dl>
 				</div>
