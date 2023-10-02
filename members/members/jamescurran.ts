3c1171800cb5b29cc8cbb3969643ee5c02993bd9
jamescurran <jamescurran@users.noreply.github.com>
2023-10-02 17:50:35 +0000

    Prettified Code!

--- members/members/jamescurran.ts
+++ members/members/jamescurran.ts
@@ -9,13 +9,13 @@ export const _EXAMPLE: MemberObject = {
 	// Everything below here is optional. By default, we pull most profile data from your GitHub profile. You can override that data here, as well as provide some additional account links below.
 	//
 	// Name - If not defined here, it will default to your display name on GitHub. If that's not defined, then your GitHub username.
-	 name: 'James Curran',
+	name: 'James Curran',
 	//
 	// Main URL - If not defined here, it will default to the website displayed on your GitHub profile. If that's not defined, then a link to your GitHub profile will be displayed.
 	// mainUrl: 'https://virtualcoffee.io',
 	//
 	// Bio - Accepts [markdown](https://spec.commonmark.org/0.30/). Please keep your bio to a reasonable length. Refer to our [members page](https://virtualcoffee.io/members/) for examples.
-	 bio: "30+ years as a developer: Assembly, C, C++ and C# (in that order) with sidelines in ASP/VBScript, ASP.Net, JavaScript, Perl, VisualBasic, plus a few others which I'm not going to mention 'cuz if I did someone might ask me to use them again (shutter).", //   - Microsoft MVP in VC++ (1994-2004) - Occasional conference speaker. - Also Licensed Massage Therapist (NJ), award-winning theater impresario, gallery exhibited photographer and published playwright. (yeah, that's right. I'm a renaissance man!)"
+	bio: "30+ years as a developer: Assembly, C, C++ and C# (in that order) with sidelines in ASP/VBScript, ASP.Net, JavaScript, Perl, VisualBasic, plus a few others which I'm not going to mention 'cuz if I did someone might ask me to use them again (shutter).", //   - Microsoft MVP in VC++ (1994-2004) - Occasional conference speaker. - Also Licensed Massage Therapist (NJ), award-winning theater impresario, gallery exhibited photographer and published playwright. (yeah, that's right. I'm a renaissance man!)"
 	//
 	// flare - If you want to add a flare to your profile, you can do so here.
 	// Uncomment the `import { profileMasks } from '../flare';` line at the top of this file to use the profileMasks.
@@ -32,22 +32,30 @@ export const _EXAMPLE: MemberObject = {
 	//
 	// Links - You can add one of each type, except website - you can add as many `website` accounts as you wish.
 	accounts: [
-		 { type: 'linkedin', username: 'jamesmcurran' },
+		{ type: 'linkedin', username: 'jamesmcurran' },
 		// { type: 'dev', username: 'yourUserName' },
 		// { type: 'codenewbie', username: 'yourUserName' },
-		 { type: 'twitter', username: 'zamescurran' },
+		{ type: 'twitter', username: 'zamescurran' },
 		// { type: 'twitch', username: 'yourUserName' },
 		// { type: 'youtube', channelId: 'yourChannelId' }, OR { type: 'youtube', customUrl: 'https://www.youtube.com/c/yourCustomUrl' },
 		// { type: 'polywork', username: 'yourUserName' },
 		// { type: 'medium', username: 'yourUserName' },
 		// { type: 'hashnode', username: 'yourUserName' },
-		{ type: 'website', url: 'https://noveltheory.com', title: 'Novel Theory.com (Resume/showcase)' },
-		{ type: 'website', url: 'https://honestillusion.com', title: 'Honest Illusion.com (Tech blog)' },
+		{
+			type: 'website',
+			url: 'https://noveltheory.com',
+			title: 'Novel Theory.com (Resume/showcase)',
+		},
+		{
+			type: 'website',
+			url: 'https://honestillusion.com',
+			title: 'Honest Illusion.com (Tech blog)',
+		},
 	],
 	badges: ['Hacktoberfest2023'],
-    location: {
-        latitute: 40.220825943631645, 
-        longitude: -74.76004035318527,
-        title: 'Trenton, NJ', // optional
-      },
+	location: {
+		latitute: 40.220825943631645,
+		longitude: -74.76004035318527,
+		title: 'Trenton, NJ', // optional
+	},
 };
