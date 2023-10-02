e7e448ebaa3379671958e80538ff96468833ec75
Alex Curtis-Slep <49458917+AlexVCS@users.noreply.github.com>
2023-10-02 13:55:38 -0400

    Update AlexVCS.ts
    fixed blunders

--- members/members/AlexVCS.ts
+++ members/members/AlexVCS.ts
@@ -42,9 +42,9 @@ export const AlexVCS: MemberObject = {
 		// { type: 'hashnode', username: 'yourUserName' },
 		// { type: 'website', url: 'https://virtualcoffee.io', title: 'Title of link' },
 	],
-	badges: ['Hacktoberfest2022'], ['Hacktoberfest2023'],
+	badges: ['Hacktoberfest2022', 'Hacktoberfest2023'],
 	location: {
-  latitute: 27.664827,
+  latitude: 27.664827,
   longitude: -81.515755,
   title: 'Florida',
 },
