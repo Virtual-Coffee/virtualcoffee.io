4c41c782c686c9eede50c00b3491be2cf23ce005
Jennifer <126206644+jenieg@users.noreply.github.com>
2023-10-02 12:02:53 -0600

    created my member page with location
    followed along with live event to create member page with location

---
+++ members/members/jenieg.ts
@@ -0,0 +1,52 @@
+import type { MemberObject } from '../types';
+// import { profileMasks } from '../flare';
+
+export const jenieg: MemberObject = {
+	// GitHub username (required)
+	github: 'jenieg',
+	//
+	// Everything below here is optional. By default, we pull most profile data from your GitHub profile. You can override that data here, as well as provide some additional account links below.
+	//
+	// Name - If not defined here, it will default to your display name on GitHub. If that's not defined, then your GitHub username.
+	name: 'Jennifer Godfrey',
+	//
+	// Main URL - If not defined here, it will default to the website displayed on your GitHub profile. If that's not defined, then a link to your GitHub profile will be displayed.
+	// mainUrl: 'https://virtualcoffee.io',
+	//
+	// Bio - Accepts [markdown](https://spec.commonmark.org/0.30/). Please keep your bio to a reasonable length. Refer to our [members page](https://virtualcoffee.io/members/) for examples.
+	bio: `I'm a passionate community taught web developer with a love for continuous learning. When I'm not working on my latest project you can find me quilting or reading the latest fantasy novel.`,
+	//
+	// flare - If you want to add a flare to your profile, you can do so here.
+	// Uncomment the `import { profileMasks } from '../flare';` line at the top of this file to use the profileMasks.
+	// So far  all we have is a profile mask, which makes your profile picture a different shape from the default square.
+	// Leave this out if you prefer the default square.
+	// You can choose from the following profile masks:
+	// profileMasks.octogon, profileMasks.hexagon, profileMasks.triangle,
+	// profileMasks.circle, profileMasks.rabbet, profileMasks.star
+	// you can also use a custom string. the profileMask values are [css clipPath values](https://developer.mozilla.org/en-US/docs/Web/CSS/clip-path)
+	flare: {
+		// example:
+		// profileMask: profileMasks.triangle,
+	},
+	//
+	// Links - You can add one of each type, except website - you can add as many `website` accounts as you wish.
+	accounts: [
+		{ type: 'linkedin', username: 'jeniemg' },
+		// { type: 'dev', username: 'yourUserName' },
+		// { type: 'codenewbie', username: 'yourUserName' },
+		// { type: 'twitter', username: 'yourUserName' },
+		// { type: 'twitch', username: 'yourUserName' },
+		// { type: 'youtube', channelId: 'yourChannelId' }, OR { type: 'youtube', customUrl: 'https://www.youtube.com/c/yourCustomUrl' },
+		// { type: 'polywork', username: 'yourUserName' },
+		// { type: 'medium', username: 'yourUserName' },
+		// { type: 'hashnode', username: 'yourUserName' },
+		// { type: 'website', url: 'https://virtualcoffee.io', title: 'Title of link' },
+	],
+	badges: ['Hacktoberfest2023'],
+  
+  location: {
+    latitute: 46.879681,
+    longitude: -110.362564,
+    title: 'Montana, USA', // optional
+  },
+};
