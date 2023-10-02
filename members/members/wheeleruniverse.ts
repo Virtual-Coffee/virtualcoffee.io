1b5f672db8430a45fc96a7071b40b28461ca10cf
wheeleruniverse <wheeleruniverse@users.noreply.github.com>
2023-10-02 19:57:57 +0000

    Prettified Code!

--- members/members/wheeleruniverse.ts
+++ members/members/wheeleruniverse.ts
@@ -4,13 +4,17 @@ export const wheeleruniverse: MemberObject = {
 	github: 'wheeleruniverse',
 	flare: {},
 	accounts: [
-    { type: 'dev', username: 'wheeleruniverse' },
-    { type: 'linkedin', username: 'wheeleruniverse' },
-    { type: 'twitter', username: 'wheeleruniverse' },
-    { type: 'website', url: 'https://linktr.ee/wheeleruniverse', title: 'Linktree' },
+		{ type: 'dev', username: 'wheeleruniverse' },
+		{ type: 'linkedin', username: 'wheeleruniverse' },
+		{ type: 'twitter', username: 'wheeleruniverse' },
+		{
+			type: 'website',
+			url: 'https://linktr.ee/wheeleruniverse',
+			title: 'Linktree',
+		},
 	],
 	badges: ['Hacktoberfest2023'],
-  location: {
+	location: {
 		latitude: 42.99633400308753,
 		longitude: -85.73938685748944,
 		title: 'Grand Rapids, MI',
