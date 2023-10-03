994bff70230e2e22609419c9931e2f9ec9bf4fbe
Justin Quinn <42960493+Justin-Quinn51@users.noreply.github.com>
2023-10-03 08:57:02 -0400

    Add profile to members page with location (#1015)
    * Add profile to members page with location
    
    * Prettified Code!
    
    ---------
    
    Co-authored-by: Justin-Quinn51 <Justin-Quinn51@users.noreply.github.com>

---
+++ members/members/JustinQuinn51.ts
@@ -0,0 +1,19 @@
+import type { MemberObject } from '../types';
+
+export const JustinQuinn51: MemberObject = {
+	github: 'Justin-Quinn51',
+	mainUrl: 'https://justinquinn.dev',
+	bio: `Full-stack engineer trying to get better, one code block at a time.`,
+	flare: {},
+	accounts: [
+		{ type: 'linkedin', username: 'justin-quinn-' },
+		{ type: 'twitter', username: 'JustinQuinn_' },
+		{ type: 'website', url: 'https://justinquinn.dev', title: 'Justin Quinn' },
+	],
+	badges: ['Hacktoberfest2023'],
+	location: {
+		latitude: 40.748718,
+		longitude: -73.985352,
+		title: 'New York, NY',
+	},
+};
