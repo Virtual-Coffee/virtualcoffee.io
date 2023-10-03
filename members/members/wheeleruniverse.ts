5b6cacddfc45b75e845a4183133ca206160e3641
Justin Wheeler <71364086+wheeleruniverse@users.noreply.github.com>
2023-10-03 08:52:25 -0400

    added me (wheeleruniverse) to members (#1010)
    * Create wheeleruniverse.ts
    
    * Prettified Code!
    
    ---------
    
    Co-authored-by: wheeleruniverse <wheeleruniverse@users.noreply.github.com>

---
+++ members/members/wheeleruniverse.ts
@@ -0,0 +1,22 @@
+import type { MemberObject } from '../types';
+
+export const wheeleruniverse: MemberObject = {
+	github: 'wheeleruniverse',
+	flare: {},
+	accounts: [
+		{ type: 'dev', username: 'wheeleruniverse' },
+		{ type: 'linkedin', username: 'wheeleruniverse' },
+		{ type: 'twitter', username: 'wheeleruniverse' },
+		{
+			type: 'website',
+			url: 'https://linktr.ee/wheeleruniverse',
+			title: 'Linktree',
+		},
+	],
+	badges: ['Hacktoberfest2023'],
+	location: {
+		latitude: 42.99633400308753,
+		longitude: -85.73938685748944,
+		title: 'Grand Rapids, MI',
+	},
+};
