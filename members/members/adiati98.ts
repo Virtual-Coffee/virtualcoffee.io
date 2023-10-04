4efd42f102e24d95a491bb2a2b56fdeceadc2483
Ayu Adiati <45172775+adiati98@users.noreply.github.com>
2023-10-04 19:49:52 +0200

    chores: Update Ayu's profile for Hacktoberfest 2023 (#1026)
    * Add Hacktoberfest2023 badge
    * Add location
    
    * Fix latitude longitude

--- members/members/adiati98.ts
+++ members/members/adiati98.ts
@@ -46,5 +46,10 @@ export const adiati98: MemberObject = {
 		{ type: 'hashnode', username: 'adiatiayu' },
 		// { type: 'website', url: 'https://virtualcoffee.io', title: 'Title of link' },
 	],
-	badges: ['Hacktoberfest2022'],
+	badges: ['Hacktoberfest2022', 'Hacktoberfest2023'],
+	location: {
+		latitude: 52.020798,
+		longitude: 4.493784,
+		title: 'Zuid Holland, Netherlands',
+	},
 };
