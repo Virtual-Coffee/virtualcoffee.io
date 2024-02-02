4776259ab5e3165f57e5d269fbee7bb3fde14443
Antriksh Dangi <132576984+antriksh-9@users.noreply.github.com>
2024-02-02 19:29:07 +0530

    Updated Nav.tsx

--- app/components/Nav.tsx
+++ app/components/Nav.tsx
@@ -81,7 +81,7 @@ export default function Nav() {
 						>
 							<svg
 								role="img"
-								viewBox="0 0 24 24"
+								viewBox="0 0 512 512"
 								width="24"
 								height="24"
 								xmlns="http://www.w3.org/2000/svg"
