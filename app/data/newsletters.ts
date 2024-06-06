f4de9034e746598ac912cb00c9108446976a4b3c
Ayu Adiati <retno.ayu98@gmail.com>
2024-06-06 22:16:08 +0200

    chore: update newsletters.ts

--- app/data/newsletters.ts
+++ app/data/newsletters.ts
@@ -1,3 +1,4 @@
+import { handle as issue202406 } from '~/routes/__frontend/newsletter/issues/2024-06';
 import { handle as issue202405 } from '~/routes/__frontend/newsletter/issues/2024-05';
 import { handle as issue202404 } from '~/routes/__frontend/newsletter/issues/2024-04';
 import { handle as issue202403 } from '~/routes/__frontend/newsletter/issues/2024-03';
@@ -41,6 +42,7 @@ import { handle as issue202102 } from '~/routes/__frontend/newsletter/issues/202
 import { handle as issue202101 } from '~/routes/__frontend/newsletter/issues/2021-01';
 
 const newsletters = [
+	{ handleData: issue202406, slug: '2024-06' },
 	{ handleData: issue202405, slug: '2024-05' },
 	{ handleData: issue202404, slug: '2024-04' },
 	{ handleData: issue202403, slug: '2024-03' },
