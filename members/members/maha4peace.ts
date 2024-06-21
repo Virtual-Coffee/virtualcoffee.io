16f41e1e60b24a91a37a7eb994826164b7b67e4c
Maha <maha.ahmed4peace@gmail.com>
2024-06-21 12:24:55 -0400

    updated maha member page to VC

--- members/members/maha4peace.ts
+++ members/members/maha4peace.ts
@@ -5,51 +5,20 @@ import type { MemberObject } from '../types';
 export const eternalmaha: MemberObject = {
 	// GitHub username (required)
 	github: 'eternalmaha',
-	//
-	// Everything below here is optional. By default, we pull most profile data from your GitHub profile. You can override that data here, as well as provide some additional account links below.
-	//
+
 	// Name - If not defined here, it will default to your display name on GitHub. If that's not defined, then your GitHub username.
-	// name: 'Maha Ahmed',
+	 name: 'Maha Ahmed',
 	//
 	// Main URL - If not defined here, it will default to the website displayed on your GitHub profile. If that's not defined, then a link to your GitHub profile will be displayed.
-	// mainUrl: 'https://maha-ahmed.netlify.app/',
+	mainUrl: 'https://maha-ahmed.netlify.app/',
 	//
 	// Bio - Accepts [markdown](https://spec.commonmark.org/0.30/). Please keep your bio to a reasonable length. Refer to our [members page](https://virtualcoffee.io/members/) for examples.
-	// bio: `Software Engineer, specializing in front end development | Community | Equity | living fiercely in the present [https://maha-ahmed.netlify.app](https://virtualcoffee.io)`,
-	//
-	// flare - If you want to add a flare to your profile, you can do so here.
-	// Uncomment the `import { profileMasks } from '../flare';` line at the top of this file to use the profileMasks.
-	// So far  all we have is a profile mask, which makes your profile picture a different shape from the default square.
-	// Leave this out if you prefer the default square.
-	// You can choose from the following profile masks:
-	// profileMasks.octogon, profileMasks.hexagon, profileMasks.triangle,
-	// profileMasks.circle, profileMasks.rabbet, profileMasks.star
-	// you can also use a custom string. the profileMask values are [css clipPath values](https://developer.mozilla.org/en-US/docs/Web/CSS/clip-path)
-	flare: {
-		// example:
-		// profileMask: profileMasks.triangle,
-	},
-	//
+	bio: `Software Engineer | Design | Community | Education | "The Eternal Now" [https://maha-ahmed.netlify.app]`,
+
 	// Links - You can add one of each type, except website - you can add as many `website` accounts as you wish.
 	accounts: [
-		// { type: 'linkedin', username: 'www.linkedin.com/in/maha-ahmed3' },
-		// { type: 'dev', username: 'yourUserName' },
-		// { type: 'codenewbie', username: 'yourUserName' },
-		// { type: 'twitter', username: 'yourUserName' },
-		// { type: 'twitch', username: 'yourUserName' },
-		// { type: 'youtube', channelId: 'yourChannelId' }, OR { type: 'youtube', customUrl: 'https://www.youtube.com/c/yourCustomUrl' },
-		// { type: 'polywork', username: 'yourUserName' },
-		// { type: 'medium', username: 'yourUserName' },
-		// { type: 'hashnode', username: 'yourUserName' },
-		// { type: 'mastodon', url: 'https://mastodon.server/@username' },
-		// { type: 'website', url: 'https://maha-ahmed.netlify.app', title: 'Portfolio for Maha Ahmed' },
+		{ type: 'linkedin', username: 'www.linkedin.com/in/maha-ahmed3' },
+		{ type: 'website', url: 'https://maha-ahmed.netlify.app', title: 'Portfolio for Maha Ahmed' },
 	],
 	badges: [],
-	// Add your location to our member map at https://virtualcoffee.io/members (optional)
-	// Feel free to be as specific or vague as you're comfortable with.
-	// location: {
-	// 	latitude: ,
-	// 	longitude: ,
-	// 	title: '', // optional
-	// },
 };
