2ea1473c8e3084f336467a11636000d948f9eb21
Dan Ott <dan@dtott.com>
2024-09-26 10:41:23 -0400

    export home page links

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,54 +9,7 @@ import { loadMdxDirectory } from '@/util/loadMdx.server';
 import { getEpisodes } from '@/data/podcast';
 import { getNewsletters } from '@/data/newsletters';
 import { getSponsors } from '@/data/sponsors';
-
-const homePageLinks = [
-	{
-		href: '/join',
-		title: 'Join Virtual Coffee',
-		description: 'Learn how to join our community',
-	},
-	{
-		href: '/about',
-		title: 'About Virtual Coffee',
-		description: `Our Mission, Core Values, History, and more.`,
-	},
-	{
-		href: '/code-of-conduct',
-		title: 'Code of Conduct',
-		description: `In order to create a welcoming and inclusive community, we ask our members to abide by our Code of Conduct.`,
-	},
-	{
-		href: '/members',
-		title: 'Our Members',
-		description: `Meet our amazing members!`,
-	},
-	{
-		href: '/monthlychallenges',
-		title: 'Monthly Challenges',
-		description: `Every month, we create a challenge for our Virtual Coffee members to complete together`,
-	},
-	{
-		href: 'https://store.virtualcoffee.io',
-		title: 'Merch Store',
-		description: `Support Virtual Coffee and show your love ❤️`,
-	},
-	{
-		href: 'https://github.com/Virtual-Coffee',
-		title: 'VC GitHub',
-		description: `Join our Open Source efforts!`,
-	},
-	{
-		href: 'https://youtube.com/c/virtualcoffeeio',
-		title: 'VC Videos',
-		description: `Recordings of Virtual Coffee Events and Live Streams.`,
-	},
-	{
-		href: 'https://twitter.com/VirtualCoffeeIO',
-		title: 'VC Twitter',
-		description: `Stay up to date and join in the fun!`,
-	},
-];
+import { homePageLinks } from '@/util/homePageLinks';
 
 export default async function Home() {
 	const resources = loadMdxDirectory({
