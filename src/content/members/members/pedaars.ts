9f0792d64d9b5afbb33edab2a2bcef70a26279d3
Aaron Pedwell <aaron.pedwell@1e.com>
2024-10-02 17:48:18 +0100

    Update pedaars.ts

--- src/content/members/members/pedaars.ts
+++ src/content/members/members/pedaars.ts
@@ -42,7 +42,7 @@ export const pedaars: MemberObject = {
 		// { type: 'hashnode', username: 'yourUserName' },
 		// { type: 'website', url: 'https://virtualcoffee.io', title: 'Title of link' },
 	],
-	badges: ['Hacktoberfest2022', 'Hacktoberfest2023'],
+	badges: ['Hacktoberfest2022', 'Hacktoberfest2023', 'Hacktoberfest2024'],
 	location: {
 		latitude: 51.27726,
 		longitude: 1.08046,
