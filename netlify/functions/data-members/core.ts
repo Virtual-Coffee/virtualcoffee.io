2d42b41ad5d06009a1f722f4654f4dd85a3fe5d2
David Akim <davidjohnakim@hotmail.com>
2024-10-03 20:39:41 -0400

    Fixed merged conflicts

---
+++ netlify/functions/data-members/core.ts
@@ -0,0 +1,4 @@
+export * from '../../../members/core/bekahhw';
+export * from '../../../members/core/danieltott';
+export * from '../../../members/core/juliaseid';
+export * from '../../../members/core/tkshill';
