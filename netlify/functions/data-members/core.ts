7956a8f1219f94a2362cd6cc7517f1324176982f
Dan Ott <dan@dtott.com>
2024-10-04 16:30:28 -0400

    remove files

--- netlify/functions/data-members/core.ts
+++
@@ -1,4 +0,0 @@
-export * from '../../../members/core/bekahhw';
-export * from '../../../members/core/danieltott';
-export * from '../../../members/core/juliaseid';
-export * from '../../../members/core/tkshill';
