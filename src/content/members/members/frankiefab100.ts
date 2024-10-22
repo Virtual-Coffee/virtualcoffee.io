8d8d68379f09423869d024d5fd025b268ea17851
Franklin Ohaegbulam <46662771+frankiefab100@users.noreply.github.com>
2024-10-22 21:01:29 +0100

    Create frankiefab100.ts (#1280)
    * Create frankiefab100.ts
    
    * Update frankiefab100.ts

---
+++ src/content/members/members/frankiefab100.ts
@@ -0,0 +1,55 @@
+import type { MemberObject } from '../types';
+import { profileMasks } from '../flare';
+
+// Change _EXAMPLE to `yourGitHubUserName` and add your info below
+export const frankiefab: MemberObject = {
+	// GitHub username (required)
+	github: 'frankiefab100',
+	//
+	// Everything below here is optional. By default, we pull most profile data from your GitHub profile. You can override that data here, as well as provide some additional account links below.
+	//
+	// Name - If not defined here, it will default to your display name on GitHub. If that's not defined, then your GitHub username.
+	name: 'Franklin Ohaegbulam',
+	//
+	// Main URL - If not defined here, it will default to the website displayed on your GitHub profile. If that's not defined, then a link to your GitHub profile will be displayed.
+	// mainUrl: 'https://virtualcoffee.io',
+	//
+	// Bio - Accepts [markdown](https://spec.commonmark.org/0.30/). Please keep your bio to a reasonable length. Refer to our [members page](https://virtualcoffee.io/members/) for examples.
+	bio: `Frontend Engineer & Designer by day | Open Sourcer by night | Technical Writer in between`,
+	//
+	// flare - If you want to add a flare to your profile, you can do so here.
+	// Uncomment the `import { profileMasks } from '../flare';` line at the top of this file to use the profileMasks.
+	// So far  all we have is a profile mask, which makes your profile picture a different shape from the default square.
+	// Leave this out if you prefer the default square.
+	// You can choose from the following profile masks:
+	// profileMasks.octogon, profileMasks.hexagon, profileMasks.triangle,
+	// profileMasks.circle, profileMasks.rabbet, profileMasks.star
+	// you can also use a custom string. the profileMask values are [css clipPath values](https://developer.mozilla.org/en-US/docs/Web/CSS/clip-path)
+	flare: {
+		// example:
+		profileMask: profileMasks.circle,
+	},
+	//
+	// Links - You can add one of each type, except website - you can add as many `website` accounts as you wish.
+	accounts: [
+		{ type: 'linkedin', username: 'frankiefab100' },
+		{ type: 'dev', username: 'frankiefab100' },
+		// { type: 'codenewbie', username: 'yourUserName' },
+		{ type: 'twitter', username: 'frankiefab100' },
+		// { type: 'twitch', username: 'yourUserName' },
+		// { type: 'youtube', channelId: 'yourChannelId' }, OR { type: 'youtube', customUrl: 'https://www.youtube.com/c/yourCustomUrl' },
+		// { type: 'polywork', username: 'yourUserName' },
+		// { type: 'medium', username: 'yourUserName' },
+		{ type: 'hashnode', username: 'frankiefab100' },
+		// { type: 'mastodon', url: 'https://mastodon.server/@username' },
+		{ type: 'website', url: 'https://bento.me/frankiefab', title: 'Bio Links' },
+	],
+	badges: ['Hacktoberfest2024'],
+	// Add your location to our member map at https://virtualcoffee.io/members (optional)
+	// Feel free to be as specific or vague as you're comfortable with.
+	// location: {
+	// 	latitude: 41.48266891706046,
+	// 	longitude: -81.80009779282766,
+	// 	title: 'Lakewood, OH', // optional
+	// },
+};
