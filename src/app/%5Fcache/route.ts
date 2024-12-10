65e5142521c61bdefd7873c69b5bf43327311c71
Dan Ott <dan@dtott.com>
2024-12-10 11:19:35 -0500

    Fix cache revalidation

--- src/app/%5Fcache/route.ts
+++ src/app/%5Fcache/route.ts
@@ -2,6 +2,8 @@ import { type NextRequest } from 'next/server';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
 
+export const dynamic = 'force-dynamic';
+
 export async function GET(request: NextRequest) {
 	const searchParams = request.nextUrl.searchParams;
 	const queryparam = searchParams.get('path');
