7031cfc03175f7b5c61212fc459ce550c8b51c85
Ayu Adiati <retno.ayu98@gmail.com>
2025-01-18 16:10:19 +0100

    feat: update monthly challenge homepage

--- src/app/monthlychallenges/page.tsx
+++ src/app/monthlychallenges/page.tsx
@@ -575,7 +575,6 @@ const challengeList: Challenge[] = [
 		),
 	},
 	{
-		current: true,
 		title: 'New Year, New Goal',
 		subtitle: `The new year offers an opportunity for a fresh start and a chance to set a new goal.`,
 		description: (
@@ -670,6 +669,32 @@ const challengeList: Challenge[] = [
 					To view all of the details of this year&apos;s challenge,{' '}
 					<Link href="/monthlychallenges/aug-2024">
 						check out the August 2024 challenge page
+					</Link>
+					.
+				</p>
+			</>
+		),
+	},
+	{
+		current: true,
+		title: 'Spring 2025 Quarter Challenge: From Idea to Stage',
+		subtitle: `Let's brainstorm those ideas, submit that CFP, and bring them to stage!`,
+		description: (
+			<>
+				<p>
+					Over three months, we'll work together to bring our internal conference to life, creating opportunities for both speakers and event organizers.
+				</p>
+				{/* <p>
+					Learn more about this challenge in{' '}
+					<a href="https://dev.to/virtualcoffee/monthly-challenge-photography-4g18">
+						this blog post
+					</a>
+					.
+				</p> */}
+				<p>
+					To view all of the details of this year&apos;s challenge,{' '}
+					<Link href="/monthlychallenges/spring-2025">
+						check out the Spring 2024 Quarter Challenge page
 					</Link>
 					.
 				</p>
