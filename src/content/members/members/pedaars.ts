8b275851a6789fa0385ef193cbd9781ae13621c2
Aaron Pedwell <aaron.pedwell@1e.com>
2025-10-01 18:51:32 +0100

    Add emoji and update badges for Aaron Pedwell (#1405)
    * Add emoji and update badges for Aaron Pedwell
    
    Add emoji and update badges for Aaron Pedwell
    
    * Prettified Code!
    
    * Update bio for member to reflect new position
    
    updated job title and company
    
    ---------
    
    Co-authored-by: pedaars <pedaars@users.noreply.github.com>

--- src/content/members/members/pedaars.ts
+++ src/content/members/members/pedaars.ts
@@ -9,12 +9,14 @@ export const pedaars: MemberObject = {
 	//
 	// Name - If not defined here, it will default to your display name on GitHub. If that's not defined, then your GitHub username.
 	name: 'Aaron Pedwell',
+	// emoji
+	emoji: '👨🏻‍💻',
 	//
 	// Main URL - If not defined here, it will default to the website displayed on your GitHub profile. If that's not defined, then a link to your GitHub profile will be displayed.
 	// mainUrl: 'https://virtualcoffee.io',
 	//
 	// Bio - Accepts [markdown](https://spec.commonmark.org/0.30/). Please keep your bio to a reasonable length. Refer to our [members page](https://virtualcoffee.io/members/) for examples.
-	bio: `Mid-level Front End Developer | 1E Limited`,
+	bio: `Front End Developer | Teamviewer`,
 	//
 	// flare - If you want to add a flare to your profile, you can do so here.
 	// Uncomment the `import { profileMasks } from '../flare';` line at the top of this file to use the profileMasks.
@@ -42,7 +44,12 @@ export const pedaars: MemberObject = {
 		// { type: 'hashnode', username: 'yourUserName' },
 		// { type: 'website', url: 'https://virtualcoffee.io', title: 'Title of link' },
 	],
-	badges: ['Hacktoberfest2022', 'Hacktoberfest2023', 'Hacktoberfest2024'],
+	badges: [
+		'Hacktoberfest2022',
+		'Hacktoberfest2023',
+		'Hacktoberfest2024',
+		'Hacktoberfest2025',
+	],
 	location: {
 		latitude: 51.27726,
 		longitude: 1.08046,
