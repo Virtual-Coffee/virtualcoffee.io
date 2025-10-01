03733e7d0826141721a0e81982092a3f2eeea512
DanUsesGitHub <DanUsesGitHub@users.noreply.github.com>
2025-10-01 17:00:03 +0000

    Prettified Code!

--- src/content/members/core/danieltott.ts
+++ src/content/members/core/danieltott.ts
@@ -46,7 +46,12 @@ export const danieltott: MemberObject = {
 		// { type: 'hashnode', username: 'yourUserName' },
 		// { type: 'website', url: 'https://virtualcoffee.io', title: 'Title of link' },
 	],
-	badges: ['Hacktoberfest2022', 'Hacktoberfest2023', 'Hacktoberfest2024', 'Hacktoberfest2025'],
+	badges: [
+		'Hacktoberfest2022',
+		'Hacktoberfest2023',
+		'Hacktoberfest2024',
+		'Hacktoberfest2025',
+	],
 	location: {
 		latitude: 41.48266891706046,
 		longitude: -81.80009779282766,
