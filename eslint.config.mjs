b1f12583da891e8e9222165222c098b74615cabe
Dan Ott <dan@dtott.com>
2025-10-14 12:07:22 -0400

    migrate config

---
+++ eslint.config.mjs
@@ -0,0 +1,35 @@
+import { defineConfig } from 'eslint/config';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+import js from '@eslint/js';
+import { FlatCompat } from '@eslint/eslintrc';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+	baseDirectory: __dirname,
+	recommendedConfig: js.configs.recommended,
+	allConfig: js.configs.all,
+});
+
+export default defineConfig([
+	{
+		extends: compat.extends('next/core-web-vitals', 'next/typescript'),
+
+		rules: {
+			'react/no-unescaped-entities': 'off',
+			'@next/next/no-html-link-for-pages': 'warn',
+		},
+	},
+	{
+		ignores: [
+			'node_modules/**',
+			'.next/**',
+			'out/**',
+			'build/**',
+			'next-env.d.ts',
+			'.netlify/**',
+			'netlify/**',
+		],
+	},
+]);
