c8a9f946d8b6038a46dd1f22ae6dcbaba9f484ec
hachemite Squalli dodoracula <66376987+hachemite@users.noreply.github.com>
2025-12-25 20:21:49 +0100

    fix: rename exported variable from _EXAMPLE to hachemite

--- src/content/members/members/hachemite.ts
+++ src/content/members/members/hachemite.ts
@@ -2,7 +2,6 @@ import { profileMasks } from '../flare';
 import type { MemberObject } from '../types';
 
 export const hachemite: MemberObject = {
-	// GitHub username (required)
     github: 'hachemite',
 	//
 	// Everything below here is optional. By default, we pull most profile data from your GitHub profile. You can override that data here, as well as provide some additional account links below.
