e75de04a3fa1584272fa7da26e6cf8bcff2e850d
hachemite Squalli dodoracula <66376987+hachemite@users.noreply.github.com>
2025-12-25 20:07:37 +0100

    feat: add hachemite to members list

---
+++ src/content/members/members/hachemite.ts
@@ -0,0 +1,25 @@
+import type { MemberObject } from '../types';
+
+export const hachemite: MemberObject = {
+    github: 'hachemite',
+
+    name: 'Hachem Squalli',
+
+
+    bio: 'An Engineering student at ENSA Fès specializing in Software Engineering and Artificial Intelligence | SecOps & MLOps Enthusiast.',
+
+
+
+    accounts: [
+        { type: 'linkedin', username: 'hachem-squalli-elhoussaini-2a7957250' },
+        { type: 'twitter', username: 'dodoracula' },
+    ],
+
+    badges: [],
+
+	location: {
+		latitude: 34.0331,
+        longitude: -5.0002,
+        title: 'Fes, Morocco',
+    },
+};
